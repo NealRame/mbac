cc18664e3a80179cc37766cd896ea12e73a43394
NealRame <contact@nealrame.com>
2014-08-23 17:28:49 +0200

    Update project layout
    - scaffold project,
    - configure grunt tasks,

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,183 @@
+path = require('path');
+
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    ///////////////////////////////////////////////////////////////////////
+    // Configure tasks
 
-    sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed'
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // assets variables
+        assets_dir: 'public',
+
+        // bower variables
+        bower_dir: 'bower_components',
+
+        // javascript variables
+        js_build_dir: 'public/js',
+        js_src_dir: 'app/client/js',
+
+        // sass variables
+        sass_build_dir: 'public/css',
+        sass_src_dir: 'app/client/scss',
+
+        // fonts variables
+        fonts_build_dir: '<%= assets_dir %>/fonts',
+
+        clean: {
+            bower: ['<%= bower_dir %>'],
+            fonts: ['<%= fonts_build_dir %>'],
+            js: ['<%= js_build_dir %>']
         },
-        files: {
-          'public/css/style.css': 'scss/style.scss'
-        }
-      }
-    },
 
-    watch: {
-      grunt: { files: ['Gruntfile.js'] },
+        bower: {
+            options: {
+                verbose: true,
+                copy: false
+            },
+            install: {
+            }
+        },
 
-      sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
-      }
-    }
-  });
+        copy: {
+            fonts: {
+                files: [
+                    {
+                        cwd: 'bower_components/foundation-icons/',
+                        dest: '<%= fonts_build_dir %>/',
+                        expand: true,
+                        filter: 'isFile',
+                        flatten: true,
+                        src: [
+                            'foundation_icons_accessibility/fonts/*',
+                            'foundation_icons_general/fonts/*',
+                            'foundation_icons_general_enclosed/fonts/*',
+                            'foundation_icons_social/fonts/*'
+                        ],
+                    }
+                ]
+            }
+        },
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+        react: {
+            components: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= js_src_dir %>/views',
+                    src: ['**/*.jsx'],
+                    dest: '<%= js_build_dir %>/views',
+                    ext: '.js'
+                }]
+            }
+        },
 
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
+        sass: {
+            icons: {
+                options: {
+                    loadPath: [
+                        'bower_components/foundation-icons/foundation_icons_accessibility/sass',
+                        'bower_components/foundation-icons/foundation_icons_general_enclosed/sass',
+                        'bower_components/foundation-icons/foundation_icons_general/sass',
+                        'bower_components/foundation-icons/foundation_icons_social/sass'
+                    ],
+                    quiet: true,
+                    style: 'compressed'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'app/client/scss',
+                    src: ['icons/*.scss'],
+                    dest: '<%= sass_build_dir %>',
+                    ext: '.css'
+                }]
+            },
+
+            style: {
+                options: {
+                    loadPath: [
+                        'bower_components/foundation/scss'
+                    ],
+                    style: 'nested'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'app/client/scss',
+                    src: ['style.scss'],
+                    dest: '<%= sass_build_dir %>',
+                    ext: '.css'
+                }]
+            },
+        },
+
+        uglify: {
+            options: {
+                compress: {
+                    sequences: true,
+                    hoist_vars: true,
+                    drop_console: true
+                },
+                output: {
+                    beautify: false,
+                    space_colon: false,
+                    bracketize: true
+                },
+                mangle: true,
+                preserveLicenseComments: true,
+                warnings: true
+            },
+            vendors: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= bower_dir %>',
+                    flatten: true,
+                    src: [
+                        'backbone/backbone.js',
+                        'jquery/dist/jquery.js',
+                        'react/react.js',
+                        'requirejs/require.js',
+                        'underscore/underscore.js'
+                    ],
+                    dest: '<%= js_build_dir %>/vendors'
+                }]
+            },
+            client: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= js_src_dir %>',
+                    src: [ '**/*.js' ],
+                    dest: '<%= js_build_dir %>',
+                    ext: '.min.js',
+                    extDot: 'first'
+                }]
+            }
+        },
+    });
+
+    ///////////////////////////////////////////////////////////////////////
+    // Load tasks plugins
+
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-react');
+
+    ///////////////////////////////////////////////////////////////////////
+    // Register macro task(s).
+
+    grunt.registerTask(
+        'assets',
+        ['bower', 'sass:icons', 'copy:fonts', 'uglify:vendors']
+    );
+
+    grunt.registerTask(
+        'style',
+        ['bower', 'sass:style']
+    );
+
+    grunt.registerTask(
+        'default',
+        ['assets', 'style']
+    );
 }
