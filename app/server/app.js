69dccaccda314157ea73e361a8d80ff6bdf42547
NealRame <contact@nealrame.com>
2014-08-25 00:47:10 +0200

    Update 'package.json'
    - fix peerDependencies issues,
    - replace deprecated static-favicon package with ‘serve-favicon’ as
    recommended,
    - set NODE_PATH env variable for start script,
    - update ‘app/server/app.js’ and ‘bin/www’ to reflect above
    modifications.

--- app/server/app.js
+++ app/server/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('routes/index');
+var users = require('routes/users');
 
 var app = express();
 
@@ -14,12 +14,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, '..', '..', 'public', 'mbac.ico')));
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
