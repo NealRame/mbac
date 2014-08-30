df6e023d9431c13d65c4a9560620308f72162364
NealRame <contact@nealrame.com>
2014-08-30 18:30:38 +0200

    Update 'app/server/app.js'
    Use Promise module to handle db connection, deport all the boilerplate
    code to app.’s

--- app/server/app.js
+++ app/server/app.js
@@ -1,62 +1,85 @@
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var debug = require('debug')('mbac:app');
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var path = require('path');
+var Promise = require('Promise');
 
-var routes = require('routes/index');
-var users = require('routes/users');
+var app;
 
-var app = express();
+exports.getInstance = function() {
+    if (app) {
+        return Promise.resolve(app);
+    }
 
-// application configuration
-app.set('config', require('config'));
+    var config = require('config');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+    return (new Promise(
+        function(resolve, reject) {
+            mongoose.connect(config.database.fullURI).connection
+                .once('error', reject)
+                .once('open', resolve);
+        })
+    ).then(function(conn) {
+        debug('Connected to db at ' + config.database.URI);
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
-app.use(favicon(path.join(__dirname, '..', '..', 'public', 'mbac.ico')));
-app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+        app = express();
 
-app.use('/', routes);
-app.use('/users', users);
+        // configuration setup
+        app.set('config', config);
+        app.set('debug', debug);
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+        // view engine setup
+        app.set('views', path.join(__dirname, 'views'));
+        app.set('view engine', 'jade');
 
-/// error handlers
+        // middleware setup
+        app.use(logger('dev'));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(cookieParser());
+        app.use(favicon(path.join(__dirname, '..', '..', 'public', 'mbac.ico')));
+        app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
+        // routes setup
+
+        app.use('/', require('routes/index'));
+        app.use('/users', require('routes/users'));
+
+        // error handlers setup
+
+        // catch 404 and forward to error handler
+        app.use(function(req, res, next) {
+            var err = new Error('Not Found');
+            err.status = 404;
+            next(err);
         });
+
+        if (app.get('env') === 'development') {
+            // development error handler
+            // will print stacktrace
+            app.use(function(err, req, res, next) {
+                res.status(err.status || 500);
+                res.render('error', {
+                    message: err.message,
+                    error: err
+                });
+            });
+        } else {
+            // production error handler
+            // no stacktraces leaked to user
+            app.use(function(err, req, res, next) {
+                res.status(err.status || 500);
+                res.render('error', {
+                    message: err.message,
+                    error: {}
+                });
+            });
+        }
+
+        return app;
     });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
+};
