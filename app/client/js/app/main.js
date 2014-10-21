6b92375170e3faca91848617c8d7924b57781c9b
NealRame <contact@nealrame.com>
2014-10-22 01:27:14 +0200

    Land page update.

--- app/client/js/app/main.js
+++ app/client/js/app/main.js
@@ -1,12 +1,11 @@
 define(function(require) {
     var $ = require('jquery');
     var Foundation = require('foundation');
-
     var stickyFooter = require('app/sticky-footer');
 
     $(document).foundation();
     $(window)
-        .bind('load', stickyFooter.bind(null, 20))
+        .bind('load', stickyFooter.bind(null, 0))
         .bind('resize', Foundation.utils.throttle(stickyFooter.bind(null, 0), 150));
-    stickyFooter(20);
+    stickyFooter(0);
 });
