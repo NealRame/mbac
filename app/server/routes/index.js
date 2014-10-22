9a343e4d3a59bfa55ba3a778b6894f9d5b6edb3b
NealRame <contact@nealrame.com>
2014-10-23 01:57:22 +0200

    Adds header to files.

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -1,9 +1,13 @@
+// index.js
+// ========
+// - author: Neal.Rame. <contact@nealrame.com>
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'mon Bar à Couture' });
+    res.render('index', { title: 'mon Bar à Couture' });
 });
 
 module.exports = router;
