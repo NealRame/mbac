4ebd299eca831c9de2d2e1d439491ee9ebfdd68c
NealRame <contact@nealrame.com>
2014-10-24 12:55:17 +0200

    Introduce admin page panels.

--- app/server/routes/admin.js
+++ app/server/routes/admin.js
@@ -3,18 +3,33 @@
 // - author: Neal.Rame. <contact@nealrame.com>
 // -   date: Wed Oct 22 22:40:49 CEST 2014
 
+var _ = require('underscore');
 var express = require('express');
 var router = express.Router();
 
+var locals = {
+    app:   'app/admin_main',
+    css:   '/css/admin_style.css',
+    title: 'mon Bar à Couture - l\'arrière boutique'
+};
+
+var process = function(res, data) {
+    res.render('admin', _.extend(data, locals));
+}
+
 /* GET admin page. */
-router.get('/', function(req, res) {
-    res.render(
-        'admin',
-        {
-            title:  'mon Bar à Couture - l\'arrière boutique',
-            script: 'app/admin_main'
-        }
-    );
-});
+router
+    .get('/', function(req, res) {
+        process(res, {});
+    })
+    .get('/blog', function(req, res) {
+        process(res, {panel: 'blog'});
+    })
+    .get('/gallery', function(req, res) {
+        process(res, {panel: 'gallery'});
+    })
+    .get('/pages', function(req, res) {
+        process(res, {panel: 'pages'});
+    })
 
 module.exports = router;
