0def3a704c6e8d399cef8f2037cb9a255b9677f7
NealRame <contact@nealrame.com>
2015-01-02 16:08:48 +0100

    Add 'files' route.

---
+++ app/server/routes/file.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var GridFs = require('gridfs-stream');
+var mongo = require('mongoose').mongo;
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/:id', function(req, res, next) {
+    var gfs = GridFs(req.db, mongo);
+
+    gfs.collection('fs')
+        .findOne(
+            {_id: new mongo.ObjectID(req.param('id'))},
+            function(err, item) {
+                if (err) {
+                    return next(err);
+                }
+
+                if (! item) {
+                    return next(Object.create(
+                            new Error('File not found'),
+                            {status: {value: 404}}
+                        )
+                    );
+                }
+
+                gfs.createReadStream({_id: req.param('id')})
+                    .pipe(res.type('image/jpeg'));
+            }
+        );
+});
+
+module.exports = router;
