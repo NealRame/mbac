38d8b8fe03e2b9d2eae789dabf83582f476c59a7
NealRame <contact@nealrame.com>
2015-01-02 15:40:02 +0100

    Move resize code from main to dashboard widget.

--- app/client/js/back/dashboard.js
+++ app/client/js/back/dashboard.js
@@ -1,15 +1,23 @@
 define(function(require) {
     var _ = require('underscore');
+    var $ = require('jquery');
     var Backbone = require('backbone');
     var Marionette = require('marionette');
     var Controller = require('back/dashboard.controller');
+    var Foundation = require('foundation');
+    var stickyFooter = require('utils/sticky-footer');
 
     console.log('-- Creating application instance');
 
     var app = new Marionette.Application();
 
     app.on('start', function() {
-        console.log('-- Dashboard started!');
+        $(document).foundation();
+        $(window)
+            .bind('load', stickyFooter.bind(null, 0))
+            .bind('resize', Foundation.utils.throttle(stickyFooter.bind(null, 0), 150));
+        stickyFooter(0);
+        console.log('-- Dashboard started');
     });
 
     console.log('-- Creating application instance - done');
--- app/client/js/back/main.js
+++ app/client/js/back/main.js
@@ -1,16 +1,6 @@
 define(function(require) {
     var $ = require('jquery');
-    var Foundation = require('foundation');
-    var stickyFooter = require('utils/sticky-footer');
     var dashboard = require('back/dashboard');
 
-    $(function() {
-        $(document).foundation();
-        $(window)
-            .bind('load', stickyFooter.bind(null, 0))
-            .bind('resize', Foundation.utils.throttle(stickyFooter.bind(null, 0), 150));
-        stickyFooter(0);
-
-        dashboard.start();
-    });
+    $(dashboard.start.bind(dashboard));
 });
