9aa53d565352bf09463428cbc35f86aa7076b24c
NealRame <contact@nealrame.com>
2015-01-16 00:46:38 +0100

    Achievenement front end page.

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -2,12 +2,26 @@
 // ========
 // - author: Neal.Rame. <contact@nealrame.com>
 
+var _ = require('underscore');
+var Achievement = require('models/achievement');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.render('index', { title: 'mon Bar à Couture' });
-});
+router
+    .get('/', function(req, res) {
+        res.render('index', {title: 'mon Bar à Couture'});
+    })
+    .get('/pages/realisations', function(req, res, next) {
+
+        Achievement.find().exec()
+            .then(function(achievements) {
+                res.locals.achievements = achievements;
+                res.render('achievements', {title: 'Réalisations'});
+            })
+            .then(null, next);
+
+
+    });
 
 module.exports = router;
