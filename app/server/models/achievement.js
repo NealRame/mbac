bf6debd47c85321eec43ba24bc6dc744550626d1
NealRame <contact@nealrame.com>
2015-01-22 13:31:15 +0100

    Comments.

--- app/server/models/achievement.js
+++ app/server/models/achievement.js
@@ -1,7 +1,7 @@
-// models/achievement.js
-// ---------------------
-// - author: Neal.Rame. <contact@nealrame.com>
-// -   date: Mar 18 nov 2014 20:11:04 CET
+/// models/Achievement
+/// ------------------
+/// - author: Neal.Rame. <contact@nealrame.com>
+/// -   date: Mar 18 nov 2014 20:11:04 CET
 
 var _ = require('underscore');
 var mongoose = require('mongoose');
@@ -13,24 +13,32 @@ var Picture = new mongoose.Schema({
     thumbnail: Schema.Types.ObjectId
 }, {_id: false});
 
+/// ### Fields
 var AchievementSchema = new Schema({
-    // Date of creation of this product
+    /// #### date
+    /// Date of creation of this product. Default value is `Date.now`.
     date: {
         type: Date,
         default: Date.now
     },
-    // The name of this product
+    /// #### name
+    /// The name of this product
     name: String,
-    // A short description of this product
+    /// #### description
+    /// A short description of this product.
     description: String,
-    // A list of picture of this product
+    /// #### pictures
+    /// A list of picture of this product.
     pictures: [Picture],
-    // Tag list for this product
+    /// #### tags
+    /// A list of tags associated to this product.
     tags: [{
         type: String,
         lowercase: true,
         trim: true
     }],
+    /// #### published
+    /// Published flag.
     published: {
         type: Boolean,
         default: false
