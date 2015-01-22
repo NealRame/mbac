ccf01e74cdc64f959390ab5e42f9fd5a59ca9183
NealRame <contact@nealrame.com>
2015-01-22 13:36:59 +0100

    Add pictures routes.

--- app/server/routes/api/index.js
+++ app/server/routes/api/index.js
@@ -3,10 +3,10 @@
 // - author: Neal.Rame. <contact@nealrame.com>
 // -   date: Mar 18 nov 2014 20:11:04 CET
 
-var _ = require('underscore');
 var express = require('express');
 var router = express.Router();
 
 router.use('/achievements', require('routes/api/achievements'));
+router.use('/pictures', require('routes/api/pictures'));
 
 module.exports = router;
