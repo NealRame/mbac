b02aa339fe1faa5c06aedcb65cd3f03a3b5dd689
NealRame <contact@nealrame.com>
2015-02-13 23:07:43 +0100

    Flags API routes.

--- app/server/routes/api/index.js
+++ app/server/routes/api/index.js
@@ -6,6 +6,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(req, res, next) {
+    req.api = true;
+    next();
+});
 router.use('/achievements', require('routes/api/achievements'));
 router.use('/pictures', require('routes/api/pictures'));
 
