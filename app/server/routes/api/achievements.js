890e24c68ab514ba234c2158868ffc23ba1f6247
NealRame <contact@nealrame.com>
2015-02-13 23:53:26 +0100

    Handle only route logics.

--- app/server/routes/api/achievements.js
+++ app/server/routes/api/achievements.js
@@ -6,16 +6,17 @@
 var _ = require('underscore');
 var Achievement = require('models/achievement');
 var async = require('async');
+var debug = require('debug')('mbac:routes.achievements');
 var express = require('express');
 var formidableGrid = require('formidable-grid');
 var gm = require('gm');
 var GridFs = require('gridfs-stream');
 var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.Types.ObjectId;
 var path = require('path');
-
 var querystring = require('querystring');
-
 var inspect = require('util').inspect;
+
 var mongo = mongoose.mongo;
 var router = express.Router();
 
@@ -29,161 +30,36 @@ function error(res, err) {
     });
 };
 
-function create_picture(gfs, file) {
+function parse_data(req) {
     var promise = new mongoose.Promise;
-
-    if (_.isString(file.thumbnail) && _.isString(file.original)) {
-        promise.fulfill({
-            original:  new mongo.ObjectID(file.original),
-            thumbnail: new mongo.ObjectID(file.thumbnail)
-        });
-        return promise;
-    }
-
-    if (! file.name) {
-        promise.error(new Error('Invalid data'));
-        return promise;
-    }
-
-    var ext = path.extname(file.name);
-    var thumb = {
-        id: new mongo.ObjectID,
-        lastModified: new Date,
-        name: path.basename(file.name, ext) + '-thumb' + ext,
-        mime: file.mime
-    };
-    var input_stream = gfs.createReadStream({
-        _id: file.id
-    });
-    var output_stream = gfs.createWriteStream({
-        _id: thumb.id,
-        filename: thumb.name,
-        contentType: thumb.mime,
-        metadata: {
-            original: file.id
-        }
-    });
-
-    output_stream
-        .once('error', promise.error.bind(promise))
-        .once('close', promise.fulfill.bind(promise, {
-            original: file.id,
-            thumbnail: thumb.id
-        }));
-
-    gm(input_stream, file.name)
-    .size({bufferStream: true}, function(err, size) {
-        if (err) {
-            return next(err);
-        }
-
-        var ratio = 1; // FIXME: read this in preferences
-        var w, h, x, y;
-
-        if (size.width >= size.height) {
-            w = size.height*ratio;
-
-            if (w > size.width) {
-                w = size.width;
-                h = w/ratio;
-            } else {
-                h = size.height;
-            }
-        } else {
-            h = size.width/ratio;
-
-            if (h > size.height) {
-                h = size.height;
-                w = size*ratio;
-            } else {
-                w = size.width;
-            }
-        }
-
-        x = (size.width - w)/2;
-        y = (size.height - h)/2;
-
-        this.crop(w, h, x, y)
-            .resize(192, 128)
-            .stream(ext)
-            .pipe(output_stream);
-    });
-
-    return promise;
-};
-
-function create_pictures(gfs, files) {
-    var promise = new mongoose.Promise;
-    async.map(
-        files,
-        function(file, next) {
-            create_picture(gfs, file).addBack(next);
-        },
-        promise.resolve.bind(promise)
-    );
-    return promise;
-};
-
-function delete_picture(gfs, picture) {
-    var promise = new mongoose.Promise;
-    async.each(
-        _.chain(picture).pick('original', 'thumbnail').values().value(),
-        function(id, next) {
-            gfs.remove({ _id: id.toString()}, next);
-        },
-        promise.resolve.bind(promise)
-    );
-    return promise;
-};
-
-function delete_pictures(gfs, pictures) {
-    var promise = new mongoose.Promise;
-    async.each(
-        pictures,
-        function(picture, next) {
-            delete_picture(gfs, picture).addBack(next);
-        },
-        promise.resolve.bind(promise)
-    );
-    return promise;
-};
-
-function parse_product_data(req) {
-    var promise = new mongoose.Promise;
-    var data = {tags: []};
-    var files = [];
+    var data = {files: [], pictures: [], tags: []};
     var form = formidableGrid(req.db, mongo, {
         accept: ['image/.*']
     });
 
+    _.bindAll(promise, 'fulfill', 'error');
+
     form
     .on('file', function(name, file) {
         if (name === 'pictures') {
-            files.push(file);
+            data.files.push({original: file.id});
         }
     })
     .on('field', function(name, value) {
         switch (name) {
         case 'pictures':
-            files.push(JSON.parse(unescape(value)));
+            data.pictures.push(new ObjectId(value));
             break;
         case 'tags':
             data.tags.push(value);
             break;
         default:
-            _.extend(data, querystring.parse(name+'='+value));
+            _.extend(data, querystring.parse(name + '=' + value));
             break;
         }
     })
-    .once('error', promise.error.bind(promise))
-    .once('end', function() {
-        create_pictures(form.gridFs, files)
-            .then(function(pictures) {
-                data.pictures = pictures;
-                promise.fulfill(data);
-            })
-            .then(null, promise.error.bind(promise));
-    })
+    .once('error', promise.error)
+    .once('end', promise.fulfill)
     .parse(req);
 
     return promise;
@@ -198,6 +74,7 @@ router
     .get('/:id', function(req, res) {
         Achievement
             .findById(req.params.id)
+            .populate('pictures')
             .exec()
             .then(function(achievement) {
                 if (achievement) {
@@ -206,6 +83,9 @@ router
                     res.sendStatus(404);
                 }
             })
+            .then(null, function(err) {
+                res.status(500).send(err);
+            });
     });
 
 router
@@ -220,10 +100,10 @@ router
         Achievement
             .findById(id)
             .exec()
-            .then(function(product) {
-                if (product) {
-                    req.product = product;
-                    req.gridFs = GridFs(req.db, mongo);
+            .then(function(achievement) {
+                if (achievement) {
+                    _.bindAll(achievement, 'path', 'destroy');
+                    req.achievement = achievement;
                     next();
                 } else {
                     throw _.extend(new Error('Not found'), {status: 404});
@@ -232,57 +112,31 @@ router
             .then(null, function(err) {
                 res.status(err.status || 500).send(err);
             });
-    })
-    .post('/', function(req, res) {
-        parse_product_data(req)
-        .then(function(data) {
-            return Achievement.create(data);
-        })
-        .then(res.send.bind(res))
-        .then(null, error.bind(null, res));
-    })
-    .route('/:id')
-        .put(function(req, res) {
-            parse_product_data(req)
+    });
+
+router
+    .route('/')
+    .post(function(req, res) {
+        parse_data(req)
             .then(function(data) {
-                // Delete the product pictures which are not present in the
-                // the data pictures
-                delete_pictures(
-                    req.gridFs,
-                    _.chain(req.product.pictures)
-                        .map(function(picture) {
-                            var test = function(other) {
-                                return picture.original.equals(other.original);
-                            }
-
-                            if (! _.some(data.pictures, test)) {
-                                return picture;
-                            }
-                        })
-                        .compact()
-                        .value()
-                );
-
-                // Update product with data
-                req.product.set(data);
-                req.product.save(function(err, product) {
-                    if (err) {
-                        console.log(err); // FIXME: handle that !
-                        error(res, err);
-                    } else {
-                        res.send(product);
-                    }
-                });
+                return Achievement.create(data);
             })
+            .then(res.send.bind(res))
             .then(null, error.bind(null, res));
-        })
-        .delete(function(req, res) {
-            delete_pictures(req.gridFs, req.product.pictures)
-                .then(function() {
-                    return Achievement.remove({_id: req.product._id}).exec();
-                })
-                .then(res.sendStatus.bind(res, 200))
-                .then(null, error.bind(null, res));
-        });
+    });
+
+router
+    .route('/:id')
+    .put(function(req, res) {
+        parse_data(req)
+            .then(req.achievement.patch)
+            .then(res.send.bind(res))
+            .then(null, error.bind(null, res));
+    })
+    .delete(function(req, res) {
+        req.achievement.destroy()
+            .then(res.sendStatus.bind(res, 200))
+            .then(null, error.bind(null, res));
+    });
 
 module.exports = router;
