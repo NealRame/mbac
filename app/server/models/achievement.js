feb2cce9f8adb4f191044b6ddd5185294a91a1ae
NealRame <contact@nealrame.com>
2015-02-13 23:52:36 +0100

    Refactor 'Achievement' model.
    - override `create` method.
    - pre `remove` middleware to delete referenced `Picture`s.
    - add `patch` method to update a document.
    - add `destroy` method to remove a document and return a `Promise`.

--- app/server/models/achievement.js
+++ app/server/models/achievement.js
@@ -4,45 +4,170 @@
 /// -   date: Mar 18 nov 2014 20:11:04 CET
 
 var _ = require('underscore');
-var mongoose = require('mongoose');
+var async = require('async');
+var debug = require('debug')('mbac:models.Achievement');
+var mongoose = require('mongoose')
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var Picture = require('models/picture');
+var Promise = mongoose.Promise;
 var Schema = mongoose.Schema;
 
-var Picture = new mongoose.Schema({
-    prefix: String,
-    original: Schema.Types.ObjectId,
-    thumbnail: Schema.Types.ObjectId
-}, {_id: false});
 
 /// ### Fields
 var AchievementSchema = new Schema({
-    /// #### date
+    /// #### Achievement#date
     /// Date of creation of this product. Default value is `Date.now`.
     date: {
+        default: Date.now,
         type: Date,
-        default: Date.now
     },
-    /// #### name
-    /// The name of this product
-    name: String,
-    /// #### description
-    /// A short description of this product.
+    /// #### Achievement#name
+    /// Name of this achievement.
+    name: {
+        trim: true,
+        type: String,
+    },
+    /// #### Achievement#description
+    /// Description of this achievement.
     description: String,
-    /// #### pictures
-    /// A list of picture of this product.
-    pictures: [Picture],
-    /// #### tags
+    /// #### Achievement#pictures
+    /// A list of reference to pictures. See [models/picture](#models.picture.md)
+    /// for more details.
+    pictures: [{
+        ref: 'Picture',
+        type: ObjectId,
+    }],
+    /// #### Achievement#tags
     /// A list of tags associated to this product.
     tags: [{
-        type: String,
         lowercase: true,
-        trim: true
+        trim: true,
+        type: String,
     }],
-    /// #### published
+    /// #### Achievement#published
     /// Published flag.
     published: {
+        default: false,
         type: Boolean,
-        default: false
     }
 });
+
+AchievementSchema.pre('remove', function(next) {
+    debug('removing', this._id);
+    async.each(
+        this.pictures,
+        function(picture, next) {
+            (picture instanceof ObjectId
+                ? Picture.findByIdAndRemove(picture).exec()
+                : picture.destroy()
+            ).addBack(next);
+        },
+        next
+    );
+});
+
+// Returns a promise of an array of pictures'ids created from the given files
+function create_pictures(files) {
+    var promise = new Promise;
+    async.map(
+        files,
+        function(file, next) {
+            Picture.create({original: file})
+                .then(function(picture) {
+                    next(null, picture._id);
+                })
+                .then(null, next);
+        },
+        promise.resolve.bind(promise)
+    );
+    return promise;
+};
+
+/// #### `Achievement.create(data, [cb])`
+/// Create an `Achievement` instance with the given data.
+///
+/// __Parameters:__
+/// - `data`.
+/// - `cb`, a node.js style callback.
+///
+/// __Returns:__
+/// - `Promise`.
+AchievementSchema.static('create', function(data, cb) {
+    var Model = this;
+    var achievement = new Model(_.pick(data, 'date', 'name', 'description', 'tags', 'published'));
+    var promise = new Promise(cb);
+
+    debug('creating Achievement', data);
+
+    _.bindAll(promise, 'resolve', 'error');
+    create_pictures(data.files)
+        .then(function(pictures) {
+            achievement.set('pictures', pictures).save(promise.resolve);
+        })
+        .then(null, promise.error);
+
+    return promise;
+});
+
+/// #### `Achievement#patch(data, [cb])`
+/// Patch this achievement with the given data.
+///
+/// __Parameters:__
+/// - `data`, data to patch this `Achievement` with.
+/// - `cb`, a node.js style callback.
+///
+/// __Returns:__
+/// - `Promise`.
+AchievementSchema.methods.patch = function(data, cb) {
+    var promise = new Promise(cb);
+
+    debug('patching', this._id);
+
+    async.compose(
+        // Patch model task
+        function(pictures, next) {
+            this.set(
+                _.pick(
+                    data, 'date', 'name', 'description', 'tags', 'published'
+                )
+            ).save(next);
+        },
+        // Create pictures task
+        function(pictures, next) {
+            create_pictures(data.files)
+                .then(function(new_pictures) {
+                    next(null, pictures.concat(new_pictures));
+                })
+                .then(null, next);
+        },
+        // Delete pictures task
+        function(next) {
+            _.chain(this.pictures).each(function(picture) {
+                var id = picture instanceof ObjectId ? picture : picture._id;
+                if (! _.any(data.pictures, id.equals.bind(id))) {
+                    Picture.findByIdAndRemove(id).exec();
+                }
+                next(null, data.pictures);
+            });
+        }
+    ).call(this, promise.resolve.bind(promise));
+
+    return promise;
+};
+
+/// #### `Achievement#destroy([cb])`
+/// Destroy this `Achievement` and all its pictures.
+///
+/// __Parameters:__
+/// - `cb`, a node.js style callback.
+///
+/// __Returns:__
+/// - `Promise`.
+AchievementSchema.methods.destroy = function(cb) {
+    // pictures are destroyed by pre-middleware on 'remove'.
+    var promise = new Promise(cb);
+    this.remove(promise.resolve.bind(promise));
+    return promise;
+};
 
 module.exports = mongoose.model('Achievement', AchievementSchema);
