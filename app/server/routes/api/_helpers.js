150a13c77f7610d6b44ac9c1bd0c98e0a7a85c12
NealRame <contact@nealrame.com>
2015-02-14 01:38:49 +0100

    Provide helpers to facilitate error handling

--- app/server/routes/api/_helpers.js
+++ app/server/routes/api/_helpers.js
@@ -1,14 +1,41 @@
 var _ = require('underscore');
+var Promise = require('mpromise');
 
-function _404(message) {
-    throw _.extend(new Error(message || 'Not found'), {status: 404});
+function error_401(message) {
+    return _.extend(new Error(message || 'Unauthorized'), {status: 401});
 };
 
-function _500(err) {
-    throw _.extend(err || new Error('Internal server error'), {status: 500});
+function error_403(message) {
+    return _.extend(new Error(message || 'Forbidden'), {status: 403});
+};
+
+function error_404(message) {
+    return _.extend(new Error(message || 'Not found'), {status: 404});
+};
+
+function error_500(err) {
+    return _.extend(err || new Error('Internal server error'), {status: 500});
 };
 
 module.exports = {
-    API404: _404,
-    API500: _500
+    throw404: function(message) { throw error_401(message); },
+    throw500: function(err) { throw error_500(err); },
+    checkIfDefined: function(value) {
+        return value || Promise.rejected(error_404());
+    },
+    authorized: function(message) {
+        return function(req, res, next) {
+            next(res.locals.loggedIn ? null : error_401(message));
+        };
+    },
+    forbidden: function(message) {
+        return function(req, res, next) {
+            next(error_403(message));
+        };
+    },
+    notFound: function(message) {
+        return function(req, res, next) {
+            next(error_404(message));
+        }
+    },
 };
