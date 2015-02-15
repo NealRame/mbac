30523a90e603489a0ce49a12483ba2600bed84cc
NealRame <contact@nealrame.com>
2015-02-15 23:59:56 +0100

    Add CRUD static methods.

--- app/server/models/achievement.js
+++ app/server/models/achievement.js
@@ -68,7 +68,7 @@ AchievementSchema.pre('remove', function(next) {
 });
 
 /// #### `Achievement.create(data, [cb])`
-/// Create an `Achievement` instance with the given data.
+/// Creates an `Achievement` instance with the given data.
 ///
 /// __Parameters:__
 /// - `data`.
@@ -77,19 +77,121 @@ AchievementSchema.pre('remove', function(next) {
 /// __Returns:__
 /// - `Promise`.
 AchievementSchema.static('create', function(data, cb) {
-    var promise = new Promise(cb);
-    var achievement = new Achievement(_.pick(data, 'date', 'name', 'description', 'tags', 'published'));
-
     debug('creating Achievement', data);
+
+    var data = data || {};
+    var promise = new Promise(cb);
+
     _.bindAll(promise, 'resolve', 'error');
-    Picture.create(data.files)
+    Picture.create(data.files || [])
         .then(function(pictures) {
-            achievement.set('pictures', pictures).save(promise.resolve);
+            var p = new Promise;
+            var achievement = new Achievement(
+                _.extend(
+                    _.pick(data, 'date', 'name', 'description', 'tags', 'published'),
+                    {pictures: pictures}
+                )
+            );
+
+            _.bindAll(p, 'resolve');
+            achievement.save(p.resolve);
+
+            return p;
+        })
+        .then(function(achievement) {
+            return achievement.populate(promise.resolve);
         })
         .then(null, promise.error);
 
     return promise;
 });
+
+/// #### `Achievement.read(id, [cb])`
+/// Returns an `Achievement` given its id.
+///
+/// __Parameters:__
+/// - `id`.
+/// - `cb`, a node.js style callback.
+///
+/// __Returns:__
+/// - `Promise`.
+AchievementSchema.static('read', function(id, cb) {
+    debug('read');
+    var promise = new Promise(cb);
+    _.bindAll(promise, 'fulfill', 'error');
+    Achievement.findById(id).exec()
+        .then(function(achievement) {
+            return achievement
+                ? achievement.populate()
+                : null;
+        })
+        .then(promise.fulfill)
+        .then(null, promise.error);
+    return promise;
+});
+
+/// #### `Achievement.readAll([cb])`
+/// Returns all `Achievement` instances.
+///
+/// __Parameters:__
+/// - `cb`, a node.js style callback.
+///
+/// __Returns:__
+/// - `Promise`.
+AchievementSchema.static('readAll', function(cb) {
+    debug('readAll');
+    var promise = new Promise(cb);
+    _.bindAll(promise, 'resolve', 'error');
+    Achievement.find().sort('-date').exec()
+        .then(function(achievements) {
+            async.map(
+                achievements,
+                function(achievement, next) {
+                    achievement.populate(next);
+                },
+                promise.resolve
+            );
+        })
+        .then(null, promise.error);
+    return promise;
+});
+
+/// #### `Achievement.patch(achievement, data, [cb])`
+/// Patch the given `Achievement` instance, with the given data.
+///
+/// __Parameters:__
+/// - `achievement`,
+/// - `data`,
+/// - `cb`, a node.js style callback.
+///
+/// __Returns:__
+/// - `Promise`.
+AchievementSchema.static('patch', function(achievement, data, cb) {
+    debug('patch');
+    var promise = new Promise(cb);
+    _.bindAll(promise, 'fulfill', 'error');
+    achievement.patch(data)
+        .then(function(achievement) {
+            return achievement.populate();
+        })
+        .then(promise.fulfill)
+        .then(null, promise.error);
+    return promise;
+});
+
+/// #### `Achievement.delete(achievement, [cb])`
+/// Deletes the given `Achievement` instance.
+///
+/// __Parameters:__
+/// - `achievement`,
+/// - `cb`, a node.js style callback.
+///
+/// __Returns:__
+/// - `Promise`.
+AchievementSchema.static('delete', function(achievement, cb) {
+    return achievement.destroy(cb);
+});
+
 
 /// #### `Achievement#populate([cb])`
 /// Populate this achievement..
