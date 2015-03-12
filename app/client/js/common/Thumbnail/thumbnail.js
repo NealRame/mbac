51270a26124245d713018c114386615ac93c080b
NealRame <contact@nealrame.com>
2015-03-12 15:32:14 +0100

    Use functional utilities.

--- app/client/js/common/Thumbnail/thumbnail.js
+++ app/client/js/common/Thumbnail/thumbnail.js
@@ -6,39 +6,10 @@ define(function(require) {
     var _ = require('underscore');
     var $ = require('jquery');
 
+    var functional = require('utils/functional');
     var AchievementThumbnailView = require('common/Thumbnail/achievement-thumbnail');
     var FileThumbnailView = require('common/Thumbnail/file-thumbnail');
     var PictureThumbnailView = require('common/Thumbnail/picture-thumbnail');
-
-    function cat() {
-        var head = _.first(arguments);
-        if (! _.isUndefined(head)) {
-            return head.concat.apply(head, _.rest(arguments));
-        }
-        return [];
-    }
-
-    function construct(head, tail) {
-        return cat([head], _.toArray(tail));
-    }
-
-    function dispatch() {
-        var funs = _.toArray(arguments);
-        var size = funs.length;
-
-        return function(model) {
-            var args = _.rest(arguments);
-
-            for (var index = 0; index < size; index++) {
-                var fun = funs[index];
-                var ret = fun.apply(fun, construct(model, args));
-
-                if (! _.isUndefined(ret)) {
-                    return ret;
-                }
-            }
-        };
-    }
 
     function hasKeys(model) {
         var keys = _.rest(arguments);
@@ -50,7 +21,7 @@ define(function(require) {
     function isa(view) {
         var keys = _.rest(arguments);
         return function(model) {
-            if (hasKeys.apply(null, construct(model, keys))) {
+            if (hasKeys.apply(null, functional.construct(model, keys))) {
                 return view;
             }
         };
@@ -60,7 +31,7 @@ define(function(require) {
         Achievement: AchievementThumbnailView,
         File: FileThumbnailView,
         Picture: PictureThumbnailView,
-        create: dispatch(
+        create: functional.dispatch(
             isa(AchievementThumbnailView, 'pictures'),
             isa(FileThumbnailView, 'file'),
             isa(PictureThumbnailView, 'original', 'thumbnail')
