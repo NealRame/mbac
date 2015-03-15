f5e0ce4d8a9dec6af83bdf10822a8411092e00c0
NealRame <contact@nealrame.com>
2015-03-15 17:27:16 +0100

    load image from `File`+ documentation.

--- app/client/js/utils/async.js
+++ app/client/js/utils/async.js
@@ -1,24 +1,20 @@
-// utils/async.js
-// --------------
-// - author: Neal.Rame. <contact@nealrame.com>
-// -   date: Wed Mar 12 03:58:30 2015
+/// utils/async.js
+/// --------------
+/// - author: Neal.Rame. <contact@nealrame.com>
+/// -   date: Wed Mar 12 03:58:30 2015
 define(function(require) {
     var $ = require('jquery');
     var Promise = require('promise');
 
-    function load_image(source) {
-        return (new Promise(function(resolve, reject) {
-            var image = new Image();
-            $(image)
-                .one('load', resolve)
-                .one('error', reject)
-                .attr('src', source);
-        })).then(function(ev) {
-            return ev.target;
-        });
-    }
-
-    function load_data_url(file) {
+    /// #### async.loadFileAsDataURL(file)
+    /// Load the file as data URL.
+    ///
+    /// __Parameters:__
+    /// - `file`, a `File` instance.
+    ///
+    /// __Return:__
+    /// - `Promise` of a `String`.
+    function load_file_as_data_url(file) {
         return (new Promise(function(resolve, reject) {
             var reader = new FileReader();
             reader.onload = resolve;
@@ -29,8 +25,32 @@ define(function(require) {
         });
     }
 
+    /// #### async.loadImage(source)
+    /// Load the file as data URL.
+    ///
+    /// __Parameters:__
+    /// - `source`, a `String` URI or a `File` instance.
+    ///
+    /// __Return:__
+    /// - `Promise` of a DOM element for an image.
+    function load_image(source) {
+        if (source instanceof File) {
+            return load_file_as_data_url(source).then(load_image);
+        } else {
+            return (new Promise(function(resolve, reject) {
+                var image = new Image();
+                $(image)
+                    .one('load', resolve)
+                    .one('error', reject)
+                    .attr('src', source);
+            })).then(function(ev) {
+                return ev.target;
+            });
+        }
+    }
+
     return {
+        loadFileAsDataURL: load_file_as_data_url,
         loadImage: load_image,
-        loadDataURL: load_data_url
     };
 });
