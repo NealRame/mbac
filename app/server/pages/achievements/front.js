d11bf3f2a55f364611a14c4af287b8d95e009edb
NealRame <contact@nealrame.com>
2015-04-06 12:41:10 +0200

    Achievement page.

--- app/server/pages/achievements/front.js
+++ app/server/pages/achievements/front.js
@@ -12,6 +12,7 @@ var Achievement = require(path.join(__dirname, 'models', 'achievement'));
 var router = express.Router();
 
 var list_template = path.join(__dirname, 'views', 'achievements.jade');
+var page_template = path.join(__dirname, 'views', 'achievement.jade');
 
 router
     // GET achievements page.
@@ -20,6 +21,17 @@ router
             .then(function(achievements) {
                 res.render(list_template, {achievements: achievements});
             })
+            .then(null, next);
+    })
+    .get('/:id', function(req, res, next) {
+        Achievement.findById(req.params.id)
+            .populate('pictures')
+            .where('published', true)
+            .exec()
+            .then(function(achievement) {
+                res.render(page_template, {achievement: achievement});
+            })
+            .then(null, next);
     });
 
 module.exports = router;
