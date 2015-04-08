1d35b516b5376c2b3a95661633bf36d14b81c101
NealRame <contact@nealrame.com>
2015-04-08 22:55:37 +0200

    Handle ui actions.
    - add a new achievement in the collection, when add button is clicked.

--- app/client/js/pages/achievements/menu.js
+++ app/client/js/pages/achievements/menu.js
@@ -7,12 +7,37 @@ define(function(require) {
     var $ = require('jquery');
     var Backbone = require('backbone');
 
+    var Achievement = require('pages/achievements/achievement');
     var template = require('text!pages/achievements/menu.html');
+
+    var channel = Backbone.Wreqr.radio.channel('global');
 
     var MenuView = Marionette.ItemView.extend({
         tagName: 'ul',
         className: 'side-nav',
+        serializeData: _.constant({}),
         template: _.template(template),
+        ui: {
+            addAchievement: '#add-achievement',
+            filterAchievement: '#filter-achievement'
+        },
+        events: {
+            'click  @ui.addAchievement': 'onAddAchievementClick',
+            'click  @ui.filterAchievement': 'onFilterAchievementClick'
+        },
+        onAddAchievementClick: function(ev) {
+            console.log('-- achievement::menu::onAddAchievementClick');
+            ev.preventDefault();
+            ev.stopPropagation();
+            this.collection.add(new Achievement());
+            return false;
+        },
+        onFilterAchievementClick: function(ev) {
+            console.log('-- achievement::menu::onFilterAchievementClick');
+            ev.preventDefault();
+            ev.stopPropagation();
+            return false;
+        }
     });
 
     return MenuView;
