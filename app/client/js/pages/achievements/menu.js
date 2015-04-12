ab960dca9c68d89feac272578f777efb34617cc1
NealRame <contact@nealrame.com>
2015-04-12 23:50:36 +0200

    Update menu.
    - start implementing tags ui.

--- app/client/js/pages/achievements/menu.js
+++ app/client/js/pages/achievements/menu.js
@@ -8,22 +8,84 @@ define(function(require) {
     var Backbone = require('backbone');
 
     var Achievement = require('pages/achievements/achievement');
-    var template = require('text!pages/achievements/menu.html');
+    var MenuTemplate = require('text!pages/achievements/menu.html');
+    var TagTemplate = require('text!pages/achievements/tag.html');
 
     var channel = Backbone.Wreqr.radio.channel('global');
 
-    var MenuView = Marionette.ItemView.extend({
+    var Tag = Backbone.Model.extend({
+        defaults: {
+            checked: false,
+            count: 0
+        },
+        inc: function() {
+            this.set('count', this.get('count') + 1);
+        },
+        toggle: function() {
+            this.set('checked', ! this.get('checked'));
+        }
+    });
+
+    var TagCollection = Backbone.Collection.extend({
+        model: Tag,
+        update: function(achievement) {
+            if (_.isArray(achievement)) {
+                _.each(achievement, this.update, this);
+            } else {
+                _.each(achievement.tags(), function(tagname) {
+                    var tag = this.add({id: tagname});
+                    tag.inc();
+                }, this);
+            }
+        }
+    });
+
+    var TagView = Marionette.ItemView.extend({
+        tagName: 'li',
+        template: _.template(TagTemplate),
+        ui: {
+            checkbox: 'input'
+        },
+        events: {
+            'change @ui.checkbox': 'onCheckboxChange'
+        },
+        initialize: function() {
+            this.listenTo(this.model, 'change', this.render);
+        },
+        onCheckboxChange: function(ev) {
+            this.model.toggle();
+        }
+    });
+
+    var TagListView = Marionette.CollectionView.extend({
+        tagName: 'ul',
+        childView: TagView,
+        initialize: function() {
+            this.collection = new TagCollection();
+        }
+    });
+
+    var MenuView = Marionette.LayoutView.extend({
         tagName: 'ul',
         className: 'side-nav',
         serializeData: _.constant({}),
-        template: _.template(template),
+        template: _.template(MenuTemplate),
+        regions: {
+            filters: '#filters'
+        },
         ui: {
             addAchievement: '#add-achievement',
-            filterAchievement: '#filter-achievement'
+            filterAchievement: '#filter-achievement',
         },
         events: {
-            'click  @ui.addAchievement': 'onAddAchievementClick',
-            'click  @ui.filterAchievement': 'onFilterAchievementClick'
+            'click @ui.addAchievement': 'onAddAchievementClick',
+            'click @ui.filterAchievement': 'onFilterAchievementClick'
+        },
+        initialize: function() {
+            this.tagListView = new TagListView();
+            this.listenToOnce(this.collection, 'sync', function() {
+                this.tagListView.collection.update(this.collection.models);
+            });
         },
         onAddAchievementClick: function(ev) {
             console.log('-- achievement::menu::onAddAchievementClick');
@@ -37,6 +99,9 @@ define(function(require) {
             ev.preventDefault();
             ev.stopPropagation();
             return false;
+        },
+        onShow: function() {
+            this.showChildView('filters', this.tagListView);
         }
     });
 
