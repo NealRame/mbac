34b3a7c077544d022b1752f25edb5f6840b9d745
NealRame <contact@nealrame.com>
2015-05-14 15:27:37 +0200

    Resize on achievement image on window `resize` event.

--- app/client/js/pages/home/main.js
+++ app/client/js/pages/home/main.js
@@ -3,6 +3,21 @@ define(function(require) {
     var $ = require('jquery');
     var async = require('common/async');
     var ui = require('common/ui');
+    var Promise = require('promise');
+
+    var __lock = true;
+
+    function lock() {
+        if (__lock) {
+            __lock = false;
+            return true;
+        }
+        return false;
+    }
+
+    function unlock() {
+        __lock = true;
+    }
 
     function map_object(o, iteratee) {
         return _.object(_.map(o, function(val, key) {
@@ -10,18 +25,38 @@ define(function(require) {
         }));
     }
 
-    $('#achievements > li.thumb .crop').each(function() {
-        var figure = $(this);
-        var uri = figure.data('uri');
-        figure.height(Math.round(2*figure.width()/3));
-        async.loadImage(uri)
-            .then(function(image) {
-                var fig_rect = map_object(ui.naturalRect(figure), function(v) {
-                    return v - 8;
-                });
-                figure.append($(image).css(
-                    ui.center(ui.cropFit(ui.naturalRect(image), fig_rect), fig_rect)
-                ));
-            });
-    });
+    function load_image(figure) {
+        return (figure.find('img').first().length > 0
+            ? Promise.resolve(figure)
+            : async.loadImage(figure.data('uri'))
+                .then(function(image) {
+                    figure.append(image);
+                    return figure;
+                })
+        );
+    }
+
+    function fit_image(figure) {
+        figure.show().height(Math.round(2*figure.width()/3));
+        var image = figure.find('img');
+        var fig_rect = map_object(ui.naturalRect(figure), function(v) {
+            return v - 8;
+        });
+        $(image).css(
+            ui.center(ui.cropFit(ui.naturalRect(image), fig_rect), fig_rect)
+        );
+        return figure;
+    }
+
+    function update_figures() {
+        if (lock()) {
+            Promise.all(_.map($('#achievements > li.thumb .crop'), function(fig) {
+                var figure = $(fig);
+                return load_image(figure).then(fit_image);
+            })).then(unlock);
+        }
+    }
+
+    update_figures();
+    $(window).bind('resize', Foundation.utils.throttle(update_figures, 150));
 });
