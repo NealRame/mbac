3fce22119cd6d747d96cd17435a4e6678db441b7
NealRame <contact@nealrame.com>
2015-05-16 23:39:18 +0200

    Let subclass overwritte the `center` method.

--- app/client/js/common/ThumbnailList/thumbnail-list.js
+++ app/client/js/common/ThumbnailList/thumbnail-list.js
@@ -11,22 +11,6 @@ define(function(require) {
 
     var Thumbnail = require('Thumbnail');
 
-    function center() {
-        var child = this.children.first();
-        if (child) {
-            this.$el.removeAttr('style');
-            var container_width = ui.rect(this.el).width;
-            var thumb_width = child.outerRect().width;
-            var thumb_count_by_row = Math.min(
-                this.collection.length,
-                Math.floor(container_width/thumb_width)
-            );
-            this.$el.css({
-                width: thumb_count_by_row*thumb_width,
-            });
-        }
-    }
-
     var ThumbnailList = Marionette.CollectionView.extend({
         className: 'thumbnails',
         tagName: 'ul',
@@ -35,19 +19,19 @@ define(function(require) {
         },
         initialize: function() {
             var ready_ = 0;
-            var center_ = _.debounce(_.bind(center, this), 150);
+            var center_ = _.debounce(_.bind(this.center, this), 150);
             this.listenTo(this, 'show', function() {
                 $(window).on('resize', center_);
             });
             this.listenTo(this, 'destroy', function() {
                 $(window).off('resize', center_);
             });
+            this.listenTo(this, 'childview:show', center_);
             this.listenTo(this, 'childview:ready', function() {
                 if (++ready_ >= this.collection.length) {
-                    Marionette.triggerMethod('ready');
+                    Marionette.triggerMethod.call(this, 'ready');
                 }
             });
-            this.listenTo(this, 'childview:show', center_);
             this.listenTo(this, 'childview:click', Marionette.triggerMethod.bind(this, 'click'));
         },
         childView: Thumbnail,
@@ -57,6 +41,21 @@ define(function(require) {
                 thumbnail_options = thumbnail_options.call(this);
             }
             return _.defaults(thumbnail_options || {}, {tagName: 'li'});
+        },
+        center: function() {
+            var child = this.children.first();
+            if (child) {
+                this.$el.removeAttr('style');
+                var container_width = ui.rect(this.el).width;
+                var thumb_width = child.outerRect().width;
+                var thumb_count_by_row = Math.min(
+                    this.collection.length,
+                    Math.floor(container_width/thumb_width)
+                );
+                this.$el.css({
+                    width: thumb_count_by_row*thumb_width,
+                });
+            }
         },
         onReady: _.noop,
         onClick: _.noop
