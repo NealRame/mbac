27787b69b75dfc17a16d7d17e11b5b6f52ca438d
NealRame <contact@nealrame.com>
2015-05-17 02:43:49 +0200

    Cache image to improve performance.

--- app/client/js/common/Thumbnail/picture-render.js
+++ app/client/js/common/Thumbnail/picture-render.js
@@ -6,25 +6,34 @@ define(function(require) {
 	var _ = require('underscore');
 	var $ = require('jquery');
 	var Backbone = require('backbone');
-	var Marionette = Backbone.Marionette;
-
+	var Marionette = require('marionette');
     var async = require('common/async');
 	var functional = require('common/functional');
 	var ui = require('common/ui');
 
+	function load_image(url) {
+		if (this._$image && this._$image.attr('src') === url) {
+			return Promise.resolve(this._$image);
+		}
+		return async.loadImage(url);
+	}
+
 	return function(model) {
 		if (functional.hasAllOfAttributes(model, 'original', 'thumbnail')) {
-			return async.loadImage(model.thumbnailURL())
+			return load_image.call(this, model.thumbnailURL())
 				.bind(this)
 				.then(function(image) {
 					var rect = this.innerRect();
-					$(image).css(ui.center(ui.cropFit(ui.naturalRect(image), rect), rect));
-					return {
-						el: image,
+					this._$image = $(image);
+					this._$image.removeAttr('style');
+					this._$image.css(ui.center(ui.cropFit(ui.naturalRect(this._$image), rect), rect));
+						var res = {
+						el: this._$image,
 						target: model.originalURL()
 					};
+					return res;
 				})
-				.catch(function() {
+				.catch(function(err) {
 					throw new Error('Failed to load image: ' + source);
 				});
 		}
