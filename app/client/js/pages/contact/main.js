cca66c069508d7e0ae84927bd5aaeadb886dac8b
NealRame <contact@nealrame.com>
2015-05-19 22:43:27 +0200

    more functional code.

--- app/client/js/pages/contact/main.js
+++ app/client/js/pages/contact/main.js
@@ -22,39 +22,78 @@ define(function(require) {
         elt.parent().find('.error').remove();
     }
 
+    function make_validator() {
+        var args = _.toArray(arguments);
+        return function(form, init, error, callback) {
+            try {
+                var data = _.reduce(args, function(memo, validator) {
+                    var $field = $('#' + validator.id, form);
+                    if ($field.length > 0 && $field.val) {
+                        init($field);
+                        var value = validator.validate($field.val(), error.bind(null, $field));
+                        if (value && memo) {
+                            memo[validator.id] = value;
+                            return memo;
+                        }
+                    } else {
+                        throw new Error(validator.id + ' is not a valid form field!');
+                    }
+                }, {});
+                if (data) callback(data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+    }
+
+    var validate = make_validator(
+        {
+            id: 'name',
+            validate: function(data, error_callback) {
+                var value = data.trim();
+                if (value.length > 0) {
+                    return value;
+                }
+                error_callback('Vous devez indiquez votre nom');
+            }
+        },
+        {
+            id: 'email',
+            validate: function(data, error_callback) {
+                var value = data.trim();
+                if (check_mail_address(value)) {
+                    return value;
+                }
+                error_callback('Adresse email invalide');
+            }
+        },
+        {
+            id: 'subject',
+            validate: function(data, error_callback) {
+                var value = data.trim();
+                if (value === 'info' || value === 'other') {
+                    return value;
+                }
+                error_callback('Valeur du champ invalide');
+            }
+        },
+        {
+            id: 'message',
+            validate: function(data, error_callback) {
+                if (data.trim().length > 0) {
+                    return data;
+                }
+                error_callback('Votre message est vide');
+            }
+        }
+    );
+
     $('#contact-form-submit').click(function(ev) {
         ev.preventDefault();
         ev.stopPropagation();
-
-        var $form = $('#contact-form');
-        var $name = $('#name', $form);
-        var name = $name.val().trim();
-        var $addr=$('#email', $form);
-        var addr=$addr.val().trim();
-        var $subject=$('#subject', $form);
-        var subject=$subject.val();
-        var $message = $('#message', $form);
-        var message = $message.val();
-
-        var ok = true;
-
-        clear_error_message($form.children());
-
-        if (name.length === 0) {
-            add_error_message($name, 'Vous devez indiquez votre nom');
-            ok = false;
-        }
-
-        if (! check_mail_address(addr)) {
-            add_error_message($addr, 'Addresse invalide');
-            ok = false;
-        }
-
-        if (message.trim().length === 0) {
-            add_error_message($message, 'Votre message est vide');
-            ok = false;
-        }
-
+        validate($('#contact-form'), clear_error_message, add_error_message, function(data) {
+            console.log(data);
+        });
         return false;
     });
 
