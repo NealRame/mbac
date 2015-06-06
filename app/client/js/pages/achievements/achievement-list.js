09c7d0ac21e04719b88fd83b71fcc8fa985f1ebb
NealRame <contact@nealrame.com>
2015-06-06 17:52:46 +0200

    Linting.

--- app/client/js/pages/achievements/achievement-list.js
+++ app/client/js/pages/achievements/achievement-list.js
@@ -3,46 +3,35 @@
 // - author: Neal.Rame. <contact@nealrame.com>
 // -   date: Fri Mar 13 18:44:07 2015
 define(function(require) {
+    'use strict';
+
     var _ = require('underscore');
-    var $ = require('jquery');
-    var Backbone = require('backbone');
-
-    var functional = require('common/functional');
-    var ui = require('common/ui');
-
-    // var AchievementEditorDialog = require('AchievementEditorDialog');
+    var Marionette = require('marionette');
     var Dialog = require('Dialog');
-    var Thumbnail = require('Thumbnail');
     var ThumbnailList = require('ThumbnailList');
     var achievement_render = require('pages/achievements/achievement-thumbnail-render');
-
-    function is_model_filterable(model) {
-        return true;
-    }
 
     return ThumbnailList.extend({
         editable: false,
         clickBehavior: 'trigger',
         childEvents: {
-            remove: 'onChildRemove',
+            remove: 'onChildRemove'
         },
         thumbnailOptions: function() {
             return {
                 removable: Marionette.getOption(this, 'editable'),
                 editable: Marionette.getOption(this, 'editable'),
                 clickBehavior: Marionette.getOption(this, 'clickBehavior'),
-                renderers: [achievement_render],
+                renderers: [achievement_render]
             };
         },
         initialize: function() {
             _.bindAll(this, 'thumbnailOptions');
             ThumbnailList.prototype.initialize.call(this);
         },
-        onChildEdit: function(view, model) {
-            console.log('-- AchievementList: edit request');
+        onChildEdit: function() {
         },
         onChildRemove: function(view, model) {
-            console.log('-- AchievementList: remove request');
             Dialog.prompt(
                 'Êtes vous sûr de supprimer cette réalisation ?',
                 {
