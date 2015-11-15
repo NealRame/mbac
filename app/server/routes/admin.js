a2753feb6f5f20c18412943172a68ba52c751e42
NealRame <contact@nealrame.com>
2015-11-15 17:19:38 +0100

    ES6 refactor.

--- app/server/routes/admin.js
+++ app/server/routes/admin.js
@@ -1,22 +1,20 @@
-// admin.js
-// ========
-// - author: Neal.Rame. <contact@nealrame.com>
-// -   date: Wed Oct 22 22:40:49 CEST 2014
+'use strict';
 
-var express = require('express');
-var router = express.Router();
+/// admin.js
+/// ========
+/// - author: Neal.Rame. <contact@nealrame.com>
+/// -   date: Wed Oct 22 22:40:49 CEST 2014
+
+const express = require('express');
+const router = express.Router();
 
 /* GET admin page. */
 router
-    .get('/', function(req, res) {
-        res.locals.page = {stylesheet: '/css/admin_style.css'};
-        res.render('admin', {
-            title: 'mon Bar à Couture - l\'arrière boutique',
-            page: {
-                name: 'admin',
-                stylesheets: ['/css/admin_style.css']
-            }
-        });
-    });
-
+    .get('/', (req, res) => res.render('admin', {
+        title: 'mon Bar à Couture - l\'arrière boutique',
+        page: {
+            name: 'admin',
+            stylesheets: ['/css/admin_style.css']
+        }
+    }));
 module.exports = router;
