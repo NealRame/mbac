555e36d79a6b1fadaecad949b9382ef58e4cd9c5
NealRame <contact@nealrame.com>
2015-11-16 16:14:19 +0100

    ES6 refactor.

--- app/server/routes/api/pictures.js
+++ app/server/routes/api/pictures.js
@@ -5,22 +5,17 @@
 /// - author: Neal.Rame. <contact@nealrame.com>
 /// -   date: Thu Jan 22 12:56:31 CET 2015
 
-const _ = require('underscore');
-
-const FormidableGrid = require('formidable-grid');
-const Picture = require('models/picture');
-
 const api = require('common/api');
 const express = require('express');
+const FormidableGrid = require('formidable-grid');
 const mongoose = require('mongoose');
+const Picture = require('models/picture');
 
 const mongo = mongoose.mongo;
 const router = express.Router();
 
 router
-    .param('id', (req, res, next, id) => Picture
-        .findById(id)
-        .exec()
+    .param('id', (req, res, next, id) => Picture.findById(id).exec()
         .then(api.exist)
         .then((picture) => {
             req.picture = picture;
@@ -31,48 +26,43 @@ router
 
 router
     .route('/')
-    .get(function(req, res, next) {
-        Picture.find().exec()
-            .then(res.send.bind(res))
-            .then(null, next);
-    });
+    .get((req, res, next) => Picture.find().exec()
+        .then((pictures) => res.send(pictures))
+        .then(null, next)
+    );
 
 router
     .route('/:id')
-    .get(function(req, res) {
-        res.send(req.picture);
-    });
+    .get((req, res) => res.send(req.picture));
 
-router.use(api.authenticationChecker());
+// router.use(api.authenticationChecker());
+
+function form_data_to_pictures_data(form_data) {
+    return (form_data
+        .filter((part) => part.field === 'files')
+        .map((part) => part.value)
+    );
+}
 
 router
     .route('/')
-    .post(function(req, res, next) {
+    .post((req, res, next) => {
         const form = new FormidableGrid(req.db, mongo, {
             accepted_mime_types: [/image\/.*/]
         });
         form.parse(req)
-            .then(function(form_data) {
-                const files = _.chain(form_data)
-                    .filter(function(part) {
-                        return part.field === 'files';
-                    })
-                    .map(_.property('value'))
-                    .value();
-                return Picture.create(files);
-            })
-            .then(function(pictures) {
-                res.send(pictures);
-            })
+            .then(form_data_to_pictures_data)
+            .then((pictures_data) => Picture.create(pictures_data))
+            .then((pictures) => res.send(pictures))
             .catch(next);
     })
     .all(api.forbidden());
 
 router
     .route('/:id')
-    .delete(function(req, res, next) {
-        req.picture.destroy()
-            .then(res.sendStatus.bind(res, 200))
+    .delete((req, res, next) => {
+        req.picture.remove()
+            .then(() => res.sendStatus(200))
             .then(null, next);
     })
     .all(api.forbidden());
