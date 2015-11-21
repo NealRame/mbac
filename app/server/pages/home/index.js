a73c396fac7c188dbed3d927aaa3f7e668f4ed5c
NealRame <contact@nealrame.com>
2015-11-22 00:29:11 +0100

    Log error.

--- app/server/pages/home/index.js
+++ app/server/pages/home/index.js
@@ -7,38 +7,34 @@
 
 const debug = require('debug')('mbac:routes:home');
 const express = require('express');
+const log_error = require('logger').error;
 const mongoose = require('mongoose');
 const path = require('path');
+
 
 const router = express.Router();
 const page_template = path.join(__dirname, 'views', 'front.jade');
 
 function last_achievement(count) {
-    return new Promise((resolve) => {
-        const Achievement = mongoose.model('Achievement');
-        if (Achievement != null) {
-            resolve([]);
-        } else {
-            Achievement.published(count)
-                .then(resolve)
-                .catch(function(err) {
-                    // TODO log err
-                    debug(err);
-                    resolve([]);
-                })
-        }
-    });
+    const Achievement = mongoose.model('Achievement');
+    return Promise.resolve(
+        Achievement != null ? Achievement.published(count) : []
+    );
 }
 
 router
     // GET achievements page.
-    .get('/', (req, res, next) => last_achievement(3)
+    .get('/', (req, res) => last_achievement(3)
         .then((achievements) => {
             debug(`-- Found ${achievements.length} last achievement(s)`);
             res.locals.achievements = achievements;
             res.render(page_template);
         })
-        .then(null, next)
+        .catch((err) => {
+            log_error(req, res, err);
+            res.locals.achievements = [];
+            res.render(page_template);
+        })
     );
 
 module.exports = {
