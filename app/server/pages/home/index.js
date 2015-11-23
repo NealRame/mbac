29dd389c3f9a8cacd27d49a849d19d7dd3ffd5f1
NealRame <contact@nealrame.com>
2015-11-23 01:17:41 +0100

    Load notifications from db.

--- app/server/pages/home/index.js
+++ app/server/pages/home/index.js
@@ -9,32 +9,52 @@ const debug = require('debug')('mbac:routes:home');
 const express = require('express');
 const log_error = require('logger').error;
 const mongoose = require('mongoose');
+const Notification = require('pages/home/models/notification');
 const path = require('path');
 
 const router = express.Router();
 const page_template = path.join(__dirname, 'views', 'front.jade');
 
-function last_achievement(count) {
+function last_achievement(req, res, count) {
     const Achievement = mongoose.model('Achievement');
-    return Promise.resolve(
-        Achievement != null ? Achievement.published(count) : []
+    if (Achievement != null) {
+        return (Achievement.published(count)
+            .then((achievements) => {
+                debug(`-- Found ${achievements.length} last achievement(s)`);
+                return achievements;
+            })
+            .catch((err) => {
+                log_error(req, res, err);
+                return [];
+            })
+        );
+    }
+    return Promise.resolve([]);
+}
+
+function active_notifications(req, res) {
+    return (Notification.active()
+        .then((notifications) => {
+            debug(`-- Found ${notifications.length} active notification(s)`);
+            return notifications;
+        })
+        .catch((err) => {
+            log_error(req, res, err);
+            return [];
+        })
     );
 }
 
 router
     // GET achievements page.
-    .get('/', (req, res) => last_achievement(3)
-        .then((achievements) => {
-            debug(`-- Found ${achievements.length} last achievement(s)`);
-            res.locals.achievements = achievements;
-            res.render(page_template);
-        })
-        .catch((err) => {
-            log_error(req, res, err);
-            res.locals.achievements = [];
-            res.render(page_template);
-        })
-    );
+    .get('/', (req, res) => {
+        Promise.all([last_achievement(req, res, 3), active_notifications(req, res)])
+            .then((result) => {
+                res.locals.achievements = result[0];
+                res.locals.notifications = result[1];
+                res.render(page_template);
+            });
+    });
 
 module.exports = {
     front: router
