0302793f4e4de3426cbd860b24a5f518031f33ba
NealRame <contact@nealrame.com>
2015-11-25 23:07:27 +0100

    Add home API controller.

---
+++ app/server/pages/home/api.js
@@ -0,0 +1,81 @@
+'use strict';
+
+/// pages/home/api.js
+/// =================
+/// - author: Neal.Rame. <contact@nealrame.com>
+/// -   date: Wed Nov 25 22:37:16 CET 2015
+
+const _ = require('underscore');
+const debug = require('debug')('mbac:routes.notifications');
+const express = require('express');
+const api = require('common/api');
+const path = require('path');
+
+const Notification = require(path.join(__dirname, 'models', 'notification'));
+const router = express.Router();
+
+function read_one(req, res) {
+    const id = req.params.id;
+    debug('-- load notification[' + id + ']');
+    return (Notification.findById(id)
+        .then(api.exist)
+        .then((notification) => (api.isAuthenticated(res) || notification.published)
+            ? notification
+            : api.error404()
+        )
+    );
+}
+
+function read_all(req, res) {
+    const query = {};
+    if (!api.isAuthenticated(res)) {
+        // Unauthorized client only get published notifications.
+        Object.assign(query, {
+            published: true
+        });
+    }
+    return (Notification.find(query).exec();
+}
+
+function notification_create(req, res, next) {
+    return Notification.create(req.body).then(res.send.bind(res), next);
+}
+
+function notification_read(req, res, next) {
+    const promise = _.isUndefined(req.params.id)
+        ? read_all(req, res)
+        : read_one(req, res);
+    promise.then(res.send.bind(res), next);
+}
+
+function notification_update(req, res, next) {
+    read_one(req, res)
+        .then((notification) => notification.update(req.body).exec())
+        .then(res.send.bind(res), next);
+}
+
+function notification_delete(req, res, next) {
+    read_one(req, res)
+        .then((notification) => notification.remove())
+        .then(res.send.bind(res), next);
+}
+
+router
+    .get('/', notification_read)
+    .get('/:id', notification_read);
+
+router
+    .use(api.authenticationChecker());
+
+router
+    .route('/')
+    .post(achievement_create)
+    .all(api.forbidden());
+
+router
+    .route('/:id')
+    .put(achievement_update)
+    .delete(achievement_delete)
+    .all(api.forbidden());
+
+module.exports = router;
---
+++ app/server/pages/home/front.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/// pages/home/front.js
+/// ===================
+/// - author: Neal.Rame. <contact@nealrame.com>
+/// -   date: Wed Nov 25 22:59:13 CET 2015
+
+const debug = require('debug')('mbac:routes:home');
+const express = require('express');
+const log_error = require('logger').error;
+const mongoose = require('mongoose');
+const Notification = require('pages/home/models/notification');
+const path = require('path');
+
+const router = express.Router();
+const page_template = path.join(__dirname, 'views', 'front.jade');
+
+function last_achievement(req, res, count) {
+    const Achievement = mongoose.model('Achievement');
+    if (Achievement != null) {
+        return (Achievement.published(count)
+            .then((achievements) => {
+                debug(`-- Found ${achievements.length} last achievement(s)`);
+                return achievements;
+            })
+            .catch((err) => {
+                log_error(req, res, err);
+                return [];
+            })
+        );
+    }
+    return Promise.resolve([]);
+}
+
+function active_notifications(req, res) {
+    return (Notification.active()
+        .then((notifications) => {
+            debug(`-- Found ${notifications.length} active notification(s)`);
+            return notifications;
+        })
+        .catch((err) => {
+            log_error(req, res, err);
+            return [];
+        })
+    );
+}
+
+router
+    // GET achievements page.
+    .get('/', (req, res) => {
+        Promise.all([last_achievement(req, res, 3), active_notifications(req, res)])
+            .then((result) => {
+                res.locals.achievements = result[0];
+                res.locals.notifications = result[1];
+                res.render(page_template);
+            });
+    });
+
+module.exports = router;
