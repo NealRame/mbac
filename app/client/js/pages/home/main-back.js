6bb410fb0ee7408c1786b211b6ae1645fff2be1e
NealRame <contact@nealrame.com>
2015-12-03 13:55:10 +0100

    Update home admin app.

--- app/client/js/pages/home/main-back.js
+++ app/client/js/pages/home/main-back.js
@@ -5,6 +5,7 @@ define(function(require) {
     var Backbone = require('backbone');
     var Marionette = require('marionette');
     var Dialog = require('Dialog');
+    var Menu = require('pages/home/menu/menu');
     var Notification = require('pages/home/notification/model');
     var NotificationView = require('pages/home/notification/view');
     var NotificationEditor = require('pages/home/notification/edit-view');
@@ -16,31 +17,57 @@ define(function(require) {
         }
     });
 
-    var collection = new (Backbone.Collection.extend({
-        model: Notification,
-        url: '/api/home/notifications'
-    }))();
+    var app_notification_channel = Backbone.Wreqr.radio.channel('notification');
 
-    var view = new NotificationCollectionView({
-        collection: collection,
-        el: $('#notifications')
+    var App = Marionette.LayoutView.extend({
+        regions: {
+            'list': '#notifications',
+            'menu': '#home-menu > .menu-content-wrapper'
+        },
+        template: false,
+        initialize: function() {
+            this.collection = new (Backbone.Collection.extend({
+                model: Notification,
+                url: '/api/home/notifications'
+            }))();
+
+            this.listView = new NotificationCollectionView({
+                collection: this.collection,
+                tagName: 'ul'
+            });
+            this.menuView = new Menu();
+
+            this.listenTo(this.collection, 'add', function(notification) {
+                NotificationEditor.open(notification);
+            });
+
+            this.listenTo(this.listView, 'childview:edit', function(view, notification) {
+                NotificationEditor.open(notification);
+            });
+            this.listenTo(this.listView, 'childView:remove', function(child_view, model) {
+                Dialog.prompt(
+                    'Êtes vous sûr de supprimer cette notification ?',
+                    {
+                        accept: function() {
+                            model.destroy();
+                        },
+                        acceptLabel: 'Oui',
+                        refuseLabel: 'Non'
+                    }
+                );
+            });
+
+            app_notification_channel.commands.setHandler('create', function() {
+                this.collection.add(new Notification());
+            }, this);
+
+            this.collection.fetch({reset: true});
+        },
+        onRender: function() {
+            this.showChildView('list', this.listView);
+            this.showChildView('menu', this.menuView);
+        }
     });
 
-    view.render();
-    view.on('childview:edit', function(child_view, model) {
-        NotificationEditor.open(model);
-    });
-    view.on('childview:remove', function(child_view, model) {
-        Dialog.prompt(
-            'Êtes vous sûr de supprimer cette notification ?',
-            {
-                accept: function() {
-                    model.destroy();
-                },
-                acceptLabel: 'Oui',
-                refuseLabel: 'Non'
-            }
-        );
-    });
-    collection.fetch();
+    (new App({el: $('body')})).render();
 });
