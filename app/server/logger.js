e9f8f9ed0c182a676fbdb9347c66776e6f37564d
NealRame <contact@nealrame.com>
2015-12-12 21:02:20 +0100

    Refactor logger modules.
    - Set collection name to logs.
    - Change log format helper.

--- app/server/logger.js
+++ app/server/logger.js
@@ -5,16 +5,18 @@
 /// author: Neal.Rame. <contact@nealrame.com>
 ///   date: Sun Nov 22 00:25:23 CET 2015
 
+require('winston-mongodb').MongoDB; // expose winston.transports.MongoDB
+
+const _ = require('underscore');
 const config = require('config');
 const onFinished = require('on-finished');
 const winston = require('winston');
-
-require('winston-mongodb').MongoDB; // expose winston.transports.MongoDB
 
 const logger = new (winston.Logger)({
     transports: [
         new (winston.transports.MongoDB)({
             capped: true,
+            collection: 'logs',
             level: 'error',
             db: config.database.URI,
             username: config.database.user,
@@ -24,28 +26,26 @@ const logger = new (winston.Logger)({
 });
 
 function format(req, res, data) {
-    const formatted_error = {url: req.originalUrl};
-    if (data instanceof Error) {
-        return Object.assign(formatted_error, {
-            message: data.message,
-            stack: data.stack
-        });
-    }
-    return Object.assign(formatted_error, data);
+    return [
+        data.message,
+        Object.assign(
+            req != null ? { url: req.originalUrl } : {},
+            _.isNumber(data.status) ? { status: data.status } : {},
+            _.isString(data.stack)  ? { stack: data.stack }: {}
+        )
+    ];
 }
 
 module.exports = {
     middleware(req, res, next) {
         onFinished(res, (err, res) => {
             if (err) {
-                logger.error('', format(req, res, err));
-            } else {
-                logger.info('', format(req, res, err));
+                logger.error.apply(logger, format(req, res, err));
             }
         });
         next();
     },
     error(req, res, err) {
-        logger.error('', format(req, res, err));
+        logger.error.apply(logger, format(req, res, err));
     }
 };
