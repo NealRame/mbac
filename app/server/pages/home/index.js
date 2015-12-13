a699cf26a580cce67a138ed003bf1ce13cff7d5f
NealRame <contact@nealrame.com>
2015-12-13 01:35:15 +0100

    Update 'app/server/pages/home/index.js'.

--- app/server/pages/home/index.js
+++ app/server/pages/home/index.js
@@ -6,10 +6,8 @@
 /// -  date:  Tue May 12 20:57:49 CEST 2015
 
 const path = require('path');
-const api_controller = require(path.join(__dirname, 'api'));
-const front_controller = require(path.join(__dirname, 'front'));
 
 module.exports = {
-    api: api_controller,
-    front: front_controller
+    api: require(path.join(__dirname, 'api')),
+    front: require(path.join(__dirname, 'front'))
 };
