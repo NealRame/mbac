8fba9e1a086395e7de1800ff262776726c938cdb
NealRame <contact@nealrame.com>
2015-12-22 13:46:18 +0100

    Update product admin app.

--- app/client/js/pages/products/back/main.js
+++ app/client/js/pages/products/back/main.js
@@ -1,4 +1,23 @@
 define(function(require) {
     'use strict';
-    console.log('Products admin app');
+
+    var $ = require('jquery');
+    var Backbone = require('backbone');
+    var Marionette = require('marionette');
+    var ApplicationLayout = require('pages/products/back/layout');
+    var ApplicationMenu = require('pages/products/back/menu/menu');
+
+    var Application = Marionette.Application.extend({
+        initialize: function() {
+            this.layout = new ApplicationLayout({
+                el: 'body'
+            });
+        },
+        onStart: function() {
+            this.layout.showChildView('menu', new ApplicationMenu());
+        }
+    });
+
+    var app = new Application();
+    app.start();
 });
