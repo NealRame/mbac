7b37c6dc7923898887bc5c2fb70f90484d9bffd1
NealRame <contact@nealrame.com>
2015-12-23 01:42:39 +0100

    routes.

--- app/client/js/pages/products/back/main.js
+++ app/client/js/pages/products/back/main.js
@@ -7,6 +7,8 @@ define(function(require) {
     var ApplicationLayout = require('pages/products/back/layout');
     var ApplicationMenu = require('pages/products/back/menu/menu');
 
+    var app_channel = Backbone.Wreqr.radio.channel('app');
+
     var Application = Marionette.Application.extend({
         initialize: function() {
             this.layout = new ApplicationLayout({
@@ -14,10 +16,32 @@ define(function(require) {
             });
         },
         onStart: function() {
+            console.log('app started');
             this.layout.showChildView('menu', new ApplicationMenu());
+            var matched = Backbone.history.start({
+                pushState: true,
+                root: '/admin/products'
+            });
+        },
+        products: function(args) {
+            console.log('app:route:/products', args);
+            app_channel.commands.execute('route', 'products', args);
+        },
+        resellers: function(args) {
+            console.log('app:route:/resellers', name, args);
+            app_channel.commands.execute('route', 'resellers', args);
         }
     });
 
     var app = new Application();
+
+    app.router = new Marionette.AppRouter({
+        controller: app,
+        appRoutes: {
+            '': 'products',
+            'products': 'products',
+            'resellers': 'resellers'
+        }
+    });
     app.start();
 });
--- app/client/js/pages/products/back/menu/menu.js
+++ app/client/js/pages/products/back/menu/menu.js
@@ -6,8 +6,11 @@ define(function(require) {
     'use strict';
 
     var _ = require('underscore');
+    var Backbone = require('backbone');
     var Marionette = require('marionette');
     var template = require('text!pages/products/back/menu/menu.html');
+
+    var app_channel = Backbone.Wreqr.radio.channel('app');
 
     return Marionette.ItemView.extend({
         tagName: 'ul',
@@ -15,21 +18,41 @@ define(function(require) {
         serializeData: _.constant({}),
         template: _.template(template),
         ui: {
+            products: '#products',
             createProduct: '#create-product',
+            resellers: '#resellers',
             createReseller: '#create-reseller'
         },
         events: {
             'click @ui.createProduct': 'onCreateProduct',
             'click @ui.createReseller': 'onCreateReseller'
         },
+        initialize: function() {
+            _.bindAll(this, 'activeProducts', 'activeResellers');
+            app_channel.commands.setHandler('route', (function(name, args) {
+                console.log(name, args);
+                // this.activeProducts();
+                // this.activeResellers();
+            }).bind(this));
+        },
+        activeProducts: function() {
+            this.ui.products.parent().addClass('active');
+            this.ui.resellers.parent().removeClass('active');
+        },
+        activeResellers: function() {
+            this.ui.resellers.parent().addClass('active');
+            this.ui.products.parent().removeClass('active');
+        },
         onCreateProduct: function(ev) {
             ev.preventDefault();
             ev.stopPropagation();
+            console.log('onCreateProduct');
             return false;
         },
         onCreateReseller: function(ev) {
             ev.preventDefault();
             ev.stopPropagation();
+            console.log('onCreateReseller');
             return false;
         }
     });
