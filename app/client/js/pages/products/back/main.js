f309c83cf2479cdac096341661c042885bf426bc
NealRame <contact@nealrame.com>
2015-12-23 14:15:29 +0100

    Fix app router.

--- app/client/js/pages/products/back/main.js
+++ app/client/js/pages/products/back/main.js
@@ -1,13 +1,20 @@
 define(function(require) {
     'use strict';
 
-    var $ = require('jquery');
     var Backbone = require('backbone');
     var Marionette = require('marionette');
     var ApplicationLayout = require('pages/products/back/layout');
     var ApplicationMenu = require('pages/products/back/menu/menu');
 
     var app_channel = Backbone.Wreqr.radio.channel('app');
+
+    var ApplicationRouter = Marionette.AppRouter.extend({
+        appRoutes: {
+            '': 'products',
+            'products': 'products',
+            'resellers': 'resellers'
+        }
+    });
 
     var Application = Marionette.Application.extend({
         initialize: function() {
@@ -16,32 +23,20 @@ define(function(require) {
             });
         },
         onStart: function() {
-            console.log('app started');
             this.layout.showChildView('menu', new ApplicationMenu());
-            var matched = Backbone.history.start({
-                pushState: true,
-                root: '/admin/products'
+            this.router = new ApplicationRouter({
+                controller: this
             });
+            Backbone.history.start();
         },
         products: function(args) {
-            console.log('app:route:/products', args);
             app_channel.commands.execute('route', 'products', args);
         },
         resellers: function(args) {
-            console.log('app:route:/resellers', name, args);
             app_channel.commands.execute('route', 'resellers', args);
         }
     });
 
     var app = new Application();
-
-    app.router = new Marionette.AppRouter({
-        controller: app,
-        appRoutes: {
-            '': 'products',
-            'products': 'products',
-            'resellers': 'resellers'
-        }
-    });
     app.start();
 });
