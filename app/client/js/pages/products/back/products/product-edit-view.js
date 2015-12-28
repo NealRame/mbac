32eccad1a5f7e7d91157b0e029f79eb83619ef29
NealRame <contact@nealrame.com>
2015-12-28 22:55:37 +0100

    Update product editor view.

--- app/client/js/pages/products/back/products/product-edit-view.js
+++ app/client/js/pages/products/back/products/product-edit-view.js
@@ -6,20 +6,35 @@ define(function(require) {
 	'use strict';
 
     var _ = require('underscore');
+	var Backbone = require('backbone');
     var Marionette = require('marionette');
+	var PictureList = require('PictureList');
     var template = require('text!pages/products/back/products/product-edit-view.html');
 
-    return Marionette.ItemView.extend({
+    return Marionette.LayoutView.extend({
         ui: {
             name: '#name',
             description: '#description',
+			price: '#price',
+			published: '#published',
             tags: '#tags'
         },
+		regions: {
+			pictures: '#pictures'
+		},
         template: _.template(template),
+		initialize: function() {
+			this.productPictureList = new PictureList({
+				collection: new Backbone.Collection(this.model.pictures())
+			});
+		},
         onRender: function() {
             this.ui.name.val(this.model.name());
             this.ui.tags.val(this.model.tags().join(', '));
             this.ui.description.val(this.model.description());
+			this.ui.published.val(this.model.published());
+			this.ui.price.val(this.model.get('price'));
+			this.showChildView('pictures', this.productPictureList);
         }
     });
 });
