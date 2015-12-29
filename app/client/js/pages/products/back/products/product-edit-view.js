19b02c7ebe1489163837755f87b309495ca5848d
NealRame <contact@nealrame.com>
2015-12-29 21:16:32 +0100

    Update style on focus.

--- app/client/js/pages/products/back/products/product-edit-view.js
+++ app/client/js/pages/products/back/products/product-edit-view.js
@@ -5,20 +5,28 @@
 define(function(require) {
 	'use strict';
 
+	var $ = require('jquery');
     var _ = require('underscore');
 	var Backbone = require('backbone');
     var Marionette = require('marionette');
 	var PictureList = require('PictureList');
+	var functional = require('common/functional');
     var template = require('text!pages/products/back/products/product-edit-view.html');
 
     return Marionette.LayoutView.extend({
+		id: 'editor-wrapper',
         ui: {
 			price: '#price',
 			published: '#published',
             description: '#description',
             name: '#name',
-            tags: '#tags'
+            tags: '#tags',
+			inputs: 'input, textarea'
         },
+		events: {
+			'focus @ui.inputs': 'onInputFocus',
+			'blur @ui.inputs': 'onInputFocus'
+		},
 		regions: {
 			pictures: '#pictures'
 		},
@@ -28,11 +36,25 @@ define(function(require) {
 				collection: new Backbone.Collection(this.model.pictures())
 			});
 		},
+		onInputFocus: functional.dispatch(
+			function(ev) {
+				if (ev.type === 'focusin') {
+					$(ev.target).parent().addClass('filled');
+					return true;
+				}
+			},
+			function(ev) {
+				if (ev.type === 'focusout') {
+					$(ev.target).parent().removeClass('filled');
+					return true;
+				}
+			}
+		),
         onRender: function() {
             this.ui.name.val(this.model.name());
             this.ui.tags.val(this.model.tags().join(', '));
             this.ui.description.val(this.model.description());
-			this.ui.published.val(this.model.published());
+			this.ui.published.prop('checked', this.model.published());
 			this.ui.price.val(this.model.get('price'));
 			this.showChildView('pictures', this.productPictureList);
         }
