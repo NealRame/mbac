b699706474982f35bc38a43c7540e3a5c97b6e6c
NealRame <contact@nealrame.com>
2016-01-13 13:48:31 +0100

    Refactoring.

--- app/client/js/pages/products/back/products/product-edit-view.js
+++ app/client/js/pages/products/back/products/product-edit-view.js
@@ -11,6 +11,8 @@ define(function(require) {
     var Marionette = require('marionette');
     var Dialog = require('Dialog');
 	var PictureList = require('PictureList');
+	var async = require('common/async');
+	var errors = require('common/errors');
 	var functional = require('common/functional');
     var template = require('text!pages/products/back/products/product-edit-view.html');
 
@@ -34,36 +36,37 @@ define(function(require) {
 			'blur @ui.inputs': 'onInputFocus',
 			'change @ui.inputs': 'onInputChanged'
 		},
+		childEvents: {
+			'remove-picture': 'onInputChanged',
+			'add-picture': 'onInputChanged'
+		},
 		regions: {
 			pictures: '#pictures'
 		},
         template: _.template(template),
+		templateHelpers: function() {
+			var model = this.model;
+			return {
+				hasError: functional.existy(this.errorMessage),
+				errorMessage: this.errorMessage,
+				fieldError: function(field) {
+					return functional.property(
+						model,
+						'validationError.reason.' + field
+					) || '';
+				},
+				fieldIsValid: function(field) {
+					return !functional.existy(functional.property(
+						model,
+						'validationError.reason.' + field
+					));
+				}
+			};
+		},
 		initialize: function(options) {
 			_.bindAll(this, 'onCommand');
 			this.router = options.router;
-			this.productPictureList = new PictureList({
-				collection: new Backbone.Collection(this.model.pictures()),
-				editable: true
-			});
-			this.listenTo(this.productPictureList, 'remove-picture', this.onInputChanged);
-			this.listenTo(this.productPictureList, 'add-picture', this.onInputChanged);
 			app_channel.commands.setHandler('product', this.onCommand);
-		},
-		values: function() {
-			return {
-				available: this.ui.available.prop('checked'),
-				description: this.ui.description.val(),
-				name: this.ui.name.val(),
-				pictures: this.productPictureList.collection.map(function(picture) {
-					return picture.attributes
-				}),
-				price: Number(this.ui.price.val()),
-				published: this.ui.published.prop('checked'),
-				resellers: [],
-				tags: this.ui.tags.val().split(',').map(function(tag) {
-					return tag.trim().toLowerCase();
-				})
-			};
 		},
 		reset: function() {
 			this.ui.name.val(this.model.name());
@@ -72,63 +75,101 @@ define(function(require) {
 			this.ui.available.prop('checked', this.model.available());
 			this.ui.published.prop('checked', this.model.published());
 			this.ui.price.val(this.model.get('price'));
-			this.showChildView('pictures', this.productPictureList);
+			this.showChildView('pictures', new PictureList({
+				collection: new Backbone.Collection(this.model.pictures()),
+				editable: true,
+				thumbnailsRect: {
+					width: 192,
+					height: 128
+				}
+			}));
 		},
-		saveProduct: function() {
-			if (this.edited) {
-				Dialog.prompt(
-					'Êtes vous sûr de vouloir sauvegarder les modificiations?',
-					{
-						accept: this.commit.bind(this),
-						acceptLabel: 'Oui',
-						refuseLabel: 'Non'
-					}
-				);
-			}
+		values: function() {
+			return {
+				available: this.ui.available.prop('checked'),
+				description: this.ui.description.val(),
+				name: this.ui.name.val(),
+				pictures: this.getChildView('pictures')
+					.items()
+					.map(function(picture) {
+						return picture.attributes
+					}),
+				price: Number(this.ui.price.val()),
+				published: this.ui.published.prop('checked'),
+				resellers: [],
+				tags: _.compact(this.ui.tags.val().split(',').map(function(tag) {
+					return tag.trim().toLowerCase();
+				}))
+			};
 		},
-		commit: function() {
-			var is_new = this.model.isNew();
+		onSave: function() {
 			var router = this.router;
-			this.collection
-				.add(this.model)
-				.set(this.values())
-				.save()
-				.then(
-					function(data) {
-						if (is_new) {
-							router.navigate('#' + data._id, {
-								replace: true
-							});
-						}
-					},
-					function(jqxhr, text_status, err) {
-						// FIXME: do something
-						console.error(err);
-					}
-				);
+			var model = this.model;
+			var view = this;
+			async.synchroniseModel(this.collection.add(model).set(this.values()))
+				.then(function() {
+					delete view.errorMessage;
+					router.navigate('#' + model.id, {
+						replace: true
+					});
+				})
+				.catch(function(err) {
+					view.errorMessage =
+						err instanceof errors.ModelValidationError
+							? 'Le formulaire contient des entrées non valides.'
+							: err.message;
+				})
+				.then(function() {
+					view.render();
+				});
 		},
-		removeProduct: function() {
-			// FIXME: implement that
-			console.error('Not implemented!');
+		onRemove: function() {
+			var router = this.router;
+			var view = this;
+			async.destroyModel(this.model)
+				.catch(function(err) {
+					view.errorMessage = err.message;
+					view.render();
+				})
+				.then(function() {
+					router.navigate('#', {
+						replace: true,
+						trigger: true
+					});
+				});
 		},
 		onCommand: functional.dispatch(
 			function(cmd) {
-				if (cmd === 'save') {
-					this.saveProduct();
+				if (cmd !== 'save') return;
+				if (this.edited) {
+					Dialog.prompt(
+						'Êtes vous sûr de vouloir sauvegarder les modificiations?',
+						{
+							accept: this.triggerMethod.bind(this, 'save'),
+							acceptLabel: 'Oui',
+							refuseLabel: 'Non'
+						}
+					);
 					return true;
 				}
 			},
 			function(cmd) {
-				if (cmd === 'remove') {
-					this.removeProduct();
-					return true;
-				}
+				if (cmd !== 'remove') return;
+				Dialog.prompt(
+					'Êtes vous sûr de vouloir supprimer ce produit?',
+					{
+						accept: this.triggerMethod.bind(this, 'remove'),
+						acceptLabel: 'Oui',
+						refuseLabel: 'Non'
+					}
+				);
+				return true;
 			}
 		),
 		onInputChanged: function() {
 			this.edited = !_.isEqual(
 				this.values(),
-				_.omit(this.model.attributes, '_id', '__v', 'date')
+				this.model.omit('_id', '__v', 'date')
 			);
 		},
 		onInputFocus: functional.dispatch(
