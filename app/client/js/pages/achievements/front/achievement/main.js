98ec796505f8e7f13b9bb02657515fd977421337
NealRame <contact@nealrame.com>
2016-01-17 13:15:29 +0100

    Update achievement app.

--- app/client/js/pages/achievements/front/achievement/main.js
+++ app/client/js/pages/achievements/front/achievement/main.js
@@ -5,9 +5,7 @@ define(function(require) {
     var $ = require('jquery');
     var Backbone = require('backbone');
     var Marionette = require('marionette');
-
-    var LightBox = require('LightBox');
-    var ThumbnailList = require('ThumbnailList');
+	var PictureList = require('PictureList');
     var AchievementBase = require('pages/achievements/common/models/achievement');
 
     var async = require('common/async');
@@ -18,39 +16,30 @@ define(function(require) {
         urlRoot: '/api/achievements/'
     });
 
-    var AchievementPictureList = ThumbnailList.extend({
-        initialize: function() {
-            ThumbnailList.prototype.initialize.call(this);
-        },
-        onClick: function(thumbnail) {
-            LightBox.open(this.collection, thumbnail._index);
-        },
-        onReady: function() {
-            ui.pushDown($('body > footer').first(), window, 0);
-        }
-    });
-
     function current_achievement_id() {
         return _.last(window.location.pathname.split('/'));
     }
 
-    var AchievementApp = Marionette.LayoutView.extend({
+    var Layout = Marionette.LayoutView.extend({
         regions: {
             'pictures-list': '#pictures-wrapper'
+        },
+        childEvents: {
+            'thumbnail-list:ready': function() {
+                ui.pushDown($('body > footer').first(), window, 0);
+            }
         },
         ui: {
             description: '#description-wrapper'
         },
         template: false,
-        initialize: function() {
-            this.achievementPictureList = new AchievementPictureList({
-                collection: new Backbone.Collection(this.model.pictures())
-            });
-        },
         onRender: function() {
             var region = this.getRegion('pictures-list');
             region.$el.empty().show();
-            region.show(this.achievementPictureList);
+            region.show(new PictureList({
+				collection: new Backbone.Collection(this.model.pictures()),
+				editable: false
+			}));
             this.ui.description
                 .empty()
                 .html(marked(this.model.description(), {headerPrefix: '__'}))
@@ -61,11 +50,11 @@ define(function(require) {
     async.fetchModel(new Achievement({
         _id: current_achievement_id()
     })).then(function(achievement) {
-        var app = new AchievementApp({
+        var layout = new Layout({
             el: $('body'),
             model: achievement
         });
-        app.render();
+        layout.render();
     }).catch(function(err) {
         console.error(err);
     });
