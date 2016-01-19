5cf8aa303823f1d5e575aab50268c62fd86408c9
NealRame <contact@nealrame.com>
2016-01-19 21:34:56 +0100

    Use `common.api` module.

--- app/server/pages/products/routes/api.js
+++ app/server/pages/products/routes/api.js
@@ -8,117 +8,36 @@
 const _ = require('underscore');
 const debug = require('debug')('mbac:api:products');
 const express = require('express');
-const FormidableGrid = require('formidable-grid');
 const api = require('common/api');
-const mongo = require('mongodb');
 const path = require('path');
-
 const models_path = path.join(__dirname, '..', 'models');
-
 const Product = require(path.join(models_path, 'product'));
 const Reseller = require(path.join(models_path, 'reseller'));
 
 const router = express.Router();
 
-// Data parsing helpers
-function get_field(data, key, transform) {
-    transform = transform || _.identity;
-    return transform(
-        _.chain(data)
-            .filter((part) => !!part && part.field === key)
-            .map(_.property('value'))
-            .value()
-    );
-}
-
-function make_object(data, attr_map) {
-    return _.object(
-        _.chain(attr_map)
-            .map((transform, key) => {
-                const value = get_field(data, key, transform);
-                if (value) {
-                    return [key, value];
-                }
-            })
-            .compact()
-            .value()
-    );
-}
-
-function create_data_parser(options) {
-    return function parser(req) {
-        const field_transformers = options.field_transformers || {};
-        const accepted_field_names = _.keys(options.field_transformers);
-        const accepted_mime_types = options.accepted_mime_types;
-        const form = new FormidableGrid(
-            req.db, mongo,
-            {accepted_mime_types, accepted_field_names}
-        );
-        return form.parse(req).then((form_data) => {
-            // FIXME: see why parse return null data.
-            return make_object(form_data, field_transformers)
-        });
-    };
-}
-
-// CRUD operations helper
-function create_CRUD_helpers(options) {
-    const Model = options.model;
-    const parse_data = options.parse_data;
-    const read_one = options.read_one;
-    const read_all = options.read_all;
-    return {
-        create(req, res, next) {
-            parse_data(req)
-                .then(Model.create)
-                .then((doc) => res.send(doc), next);
-        },
-        read(req, res, next) {
-            (_.isUndefined(req.params.id)
-                ? read_all(req, res)
-                : read_one(req, res)
-            ).then((docs) => res.send(docs), next);
-        },
-        update(req, res, next) {
-            Promise
-                .all([read_one(req, res), parse_data(req, res)])
-                .then((args) => Model.patch.apply(null, args))
-                .then((doc) => res.send(doc), next);
-        },
-        delete(req, res, next) {
-            read_one(req, res)
-                .then((doc) => Model.delete(doc))
-                .then(() => res.send({}), next)
-        }
-    }
-}
-
 // Reseller CRUD operations
-function reseller_read_one(req, res) {
-    return Reseller
-        .findOne(Object.assign(
-            {_id: req.params.id},
-            !api.isAuthenticated(res) ? {published: true} : {}
-        ))
-        .populate('pictures')
-        .exec();
-}
-
-function reseller_read_all() {
-    return Reseller
-        .find({})
-        .select('-description')
-        .populate('pictures')
-        .exec();
-}
-
-const Reseller_CRUD_helpers = create_CRUD_helpers({
+const Reseller_CRUD_helpers = api.createCRUDHelpers({
     model: Reseller,
-    read_one: reseller_read_one,
-    read_all: reseller_read_all,
-    parse_data: create_data_parser({
+    read_one(req, res) {
+        return Reseller
+            .findOne(Object.assign(
+                {_id: req.params.id},
+                !api.isAuthenticated(res) ? {published: true} : {}
+            ))
+            .populate('pictures')
+            .exec();
+    },
+    read_all() {
+        return Reseller
+            .find({})
+            .select('-description')
+            .populate('pictures')
+            .exec();
+    },
+    transform_data: api.createFormDataParser({
         accepted_mime_types: [/image\/.*/],
-        field_transformers: {
+        fields: {
             address: _.first,
             description: _.first,
             files: _.identity,
@@ -149,39 +68,35 @@ router
     .all(api.forbidden());
 
 // Product CRUD operations
-function product_read_one(req, res) {
-    const id = req.params.id;
-    debug('-- load product[' + id + ']');
-    return Product
-        .findOne(Object.assign(
-            { _id: id },
-            !api.isAuthenticated(res) ? {published: true} : {}
-        ))
-        .populate('pictures')
-        .populate('resellers', '-description')
-        .exec()
-        .then(api.exist);
-}
-
-function product_read_all(req, res) {
-    debug('-- load all products');
-    return Product
-        .find(Object.assign(
-            !api.isAuthenticated(res) ? {published: true} : {},
-            !api.isAuthenticated(res) ? {pictures: {$not: {$size: 0}}} : {}
-        ))
-        .populate('pictures')
-        .populate('resellers', '-description')
-        .exec();
-}
-
-const Product_CRUD_helpers = create_CRUD_helpers({
+const Product_CRUD_helpers = api.createCRUDHelpers({
     model: Product,
-    read_one: product_read_one,
-    read_all: product_read_all,
-    parse_data: create_data_parser({
+    read_one(req, res) {
+        const id = req.params.id;
+        debug('-- load product[' + id + ']');
+        return Product
+            .findOne(Object.assign(
+                { _id: id },
+                !api.isAuthenticated(res) ? {published: true} : {}
+            ))
+            .populate('pictures')
+            .populate('resellers', '-description')
+            .exec()
+            .then(api.exist);
+    },
+    read_all(req, res) {
+        debug('-- load all products');
+        return Product
+            .find(Object.assign(
+                !api.isAuthenticated(res) ? {published: true} : {},
+                !api.isAuthenticated(res) ? {pictures: {$not: {$size: 0}}} : {}
+            ))
+            .populate('pictures')
+            .populate('resellers', '-description')
+            .exec();
+    },
+    transform_data: api.createFormDataParser({
         accepted_mime_types: [/image\/.*/],
-        field_transformers: {
+        fields: {
             available: _.first,
             description: _.first,
             files: _.identity,
