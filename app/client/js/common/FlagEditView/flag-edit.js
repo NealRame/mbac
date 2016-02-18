226b31ffdd1a4ea67db12b7ac13f3582aa4bb13f
NealRame <contact@nealrame.com>
2016-02-18 22:32:05 +0100

    Add FlagEditView module.

---
+++ app/client/js/common/FlagEditView/flag-edit.js
@@ -0,0 +1,57 @@
+// FlagEditView.
+// =============
+// - author: Neal.Rame. <contact@nealrame.com>
+// -   date: Thu Feb 18 22:28:47 CET 2016
+define(function(require) {
+    'use strict';
+
+    var _ = require('underscore');
+    var Marionette = require('marionette');
+    var util = require('common/util');
+    var template = require('text!common/FlagEditView/flag-edit.html');
+
+    return Marionette.ItemView.extend({
+        template: _.template(template),
+        className: 'row',
+        ui: {
+            input: '.switch > input'
+        },
+        events: {
+            'change @ui.input': 'onInputChanged',
+            'blur @ui.input': 'onInputLostFocus',
+            'focus @ui.input': 'onInputGainFocus'
+        },
+        initialize: function(options) {
+            this.mergeOptions(options, ['inputAttribute', 'inputId', 'inputLabel']);
+            if (!this.inputId) {
+                this.inputId = util.randomString({prefix: 'input'});
+            }
+        },
+        serializeData: function() {
+            return {
+                inputId: this.inputId,
+                inputLabel: this.inputLabel
+            };
+        },
+        onInputChanged: function() {
+            this.triggerMethod('change', this.inputAttribute, this.ui.input.val());
+        },
+        onInputGainFocus: function() {
+            this.ui.input.parent().addClass('focus');
+        },
+        onInputLostFocus: function() {
+            this.ui.input.parent().removeClass('focus');
+        },
+        value: function() {
+            return _.object([
+                [
+                    this.inputAttribute,
+                    this.ui.input.prop('checked')
+                ]
+            ]);
+        },
+        setValue: function(value) {
+            this.ui.input.prop('checked', value);
+        }
+    });
+});
