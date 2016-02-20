444872a8d82302e3b9ed5ce1955ee860cccbe611
NealRame <contact@nealrame.com>
2016-02-21 00:46:03 +0100

    Finish implementation of TagEditView.

--- app/client/js/common/TagsEditView/tags-edit.js
+++ app/client/js/common/TagsEditView/tags-edit.js
@@ -6,9 +6,33 @@ define(function(require) {
     'use strict';
 
     var _ = require('underscore');
+    var Backbone = require('backbone');
     var Marionette = require('marionette');
+    var functional = require('common/functional');
     var util = require('common/util');
     var template = require('text!common/TagsEditView/tags-edit.html');
+    var TagListView = require('common/TagsEditView/tag-list');
+    var AutocompleteItem = require('common/TagsEditView/autocomplete-list');
+
+    var KEY_UP = 38;
+    var KEY_DOWN = 40;
+    var KEY_ESC = 27;
+    var KEY_ENTER = 13;
+
+    function autocomplete(prefix, available_tags, collection) {
+        if (prefix.length > 0) {
+            return _.chain(available_tags)
+                .difference(collection.pluck('tag'))
+                .filter(function(tag) {
+                    return tag.startsWith(prefix);
+                })
+                .map(function(tag) {
+                    return {item: tag};
+                })
+                .value();
+        }
+        return [];
+    }
 
     return Marionette.LayoutView.extend({
         className: 'row',
@@ -17,20 +41,27 @@ define(function(require) {
             inputWrapper: '.input'
         },
         regions: {
-            tagList: '.tag-list-wrapper'
+            tagList: '.tag-list-wrapper',
+            autocompleteList: '.autocomplete-list-wrapper'
         },
         events: {
-            'change @ui.input': 'onInputChanged',
+            'keyup @ui.input': 'onInputChanged',
             'blur @ui.input': 'onInputLostFocus',
             'focus @ui.input': 'onInputGainFocus'
         },
         template: _.template(template),
         initialize: function(options) {
-            this.mergeOptions(options, ['inputAttribute', 'inputId', 'inputLabel']);
-            if (!this.inputId) {
+            this.tags = new Backbone.Collection([]);
+            this.mergeOptions(options, ['inputAttribute', 'inputAvailableTags', 'inputId', 'inputLabel']);
+            if (!functional.existy(this.inputId)) {
                 this.inputId = util.randomString({
                     prefix: 'input'
                 });
+            }
+            if (!functional.existy(this.inputAvailableTags)) {
+                this.inputAvailableTags = [];
+            } else {
+                this.inputAvailableTags = this.inputAvailableTags.split(',');
             }
         },
         serializeData: function() {
@@ -39,8 +70,31 @@ define(function(require) {
                 inputLabel: this.inputLabel
             };
         },
-        onInputChanged: function() {
-            this.triggerMethod('changed');
+        onInputChanged: function(ev) {
+            var key_code = ev.keyCode;
+            var prefix = this.ui.input.val().trim().toLowerCase();
+            if (key_code === KEY_UP || key_code === KEY_DOWN) {
+                this.ui.input.val(this.getRegion('autocompleteList').currentView.next(key_code));
+                ev.stopPropagation();
+                ev.preventDefault();
+            } else if (key_code === KEY_ESC) {
+                this.getRegion('autocompleteList').empty();
+                this.ui.input.val(this.ui.input.data('value'));
+                this.ui.input.removeData('value');
+                ev.stopPropagation();
+                ev.preventDefault();
+            } else if (prefix.length > 0 && key_code === KEY_ENTER) {
+                this.getRegion('autocompleteList').empty();
+                this.ui.input.val('');
+                this.tags.add({tag: prefix});
+            } else if (prefix.length > 0) {
+                this.ui.input.data('value', this.ui.input.val());
+                this.showChildView('autocompleteList', new AutocompleteItem({
+                    collection: new Backbone.Collection(autocomplete(prefix, this.inputAvailableTags, this.tags))
+                }));
+            } else {
+                this.getRegion('autocompleteList').empty();
+            }
         },
         onInputGainFocus: function() {
             this.ui.inputWrapper.addClass('focus');
@@ -48,16 +102,23 @@ define(function(require) {
         onInputLostFocus: function() {
             this.ui.inputWrapper.removeClass('focus');
         },
+        onRender: function() {
+            this.showChildView('tagList', new TagListView({
+                collection: this.tags
+            }));
+        },
         value: function() {
             return _.object([
                 [
                     this.inputAttribute,
-                    []
+                    this.tags.pluck('tag')
                 ]
             ]);
         },
         setValue: function(value) {
-            this.ui.input.val(value);
+            this.tags.add(_.map(value, function(tag) {
+                return {tag: tag};
+            }));
         }
     });
 });
